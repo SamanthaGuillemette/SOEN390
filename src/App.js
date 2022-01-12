5de984b6bac2e42d0450298601a83966eabbe72f
Quang Tran <nhatquang304@gmail.com>
2022-01-12 15:46:40 -0500

    add bare minimal starting files for project

---
+++ src/App.js
@@ -0,0 +1,30 @@
+// This is a starting point (driver) of our app
+
+function App() {
+  // ======================================================================
+  //
+  // All the code logic will stay in this section.
+  //
+  // ======================================================================
+
+
+  return (
+    // ==================================================================
+    //
+    // Evrything inside 'return' will be rendered out as HTML.
+    // But the code below is actually called JSX.
+    //
+    <div>
+      <h1>Hello React</h1>
+      <p>This is a dummy piece of text</p>
+    </div>
+    //
+    //
+    // ==================================================================
+  );
+}
+
+
+// By default, JS will make this file private, so we need to export it in order
+// to use in (by import) in other files.
+export default App;
