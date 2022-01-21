a8ed90d01d279caf561619b377f9427c44317f51
Laila Alhalabi <70773705+lailaalhalabi@users.noreply.github.com>
2022-01-21 12:39:18 -0500

    Update index.js

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,9 +1,32 @@
+import * as React from "react";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import './signin.css';
 const SignIn = () => {
   return (
-    <div>
-      <h1>SignIn page</h1>
+    <div >
+      <div className="Login">
+        <h3>SignIn to Continue</h3>
+        <form className="LoginForm">
+          <TextField
+            required
+            id="outlined-required"
+            label="Email"
+            type="email" />
+          <br /><br />
+          <TextField
+            required
+            id="outlined-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password" />
+          <br /><br />
+          <Button variant="contained">Login</Button>
+          <br />
+          <p>Forgot your password? <a href="#"> click here!</a> </p>
+        </form>
+      </div>
     </div>
   );
 };
-
 export default SignIn;
