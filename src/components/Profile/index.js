f3bd5a2f19b09621a0aff362e1dac20871d6d14a
Samantha Guillemette <samantha.guillemette@gmail.com>
2022-01-21 16:40:00 -0500

    Avatar and main box sizing #37

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,21 +1,19 @@
 import "./profile.css";
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
-import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
 function Profile() {
 
   return (
-    <React.Fragment>
-    <CssBaseline />
-    <Avatar id="avatar" src="/broken-image.jpg" />
-    <Container id="container" maxWidth="md">
-      <Box sx={{ bgcolor: '#e8e8e8', width: '135vh' , height: '100vh' }} />
+   <Container id="container" maxWidth="md">
+    
+    
+      <Box  sx={{ flexDirection:'column', maxHeight:3/4}}><Avatar sx={{maxHeight:'md'}} id="avatar" src="/broken-image.jpg" /></Box>
+      <Box sx={{ flexDirection: 'column', bgcolor: '#e8e8e8', width: '135vh' , height: '100vh' }} />
     </Container>
-  </React.Fragment>
+  
   );
 }
 
