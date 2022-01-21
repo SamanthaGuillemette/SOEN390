76d7e7bd9d969e9c323edaa31112892407656380
salehatrq <68137308+salehatrq@users.noreply.github.com>
2022-01-21 18:09:46 -0500

    Added basic info for a fictional patient #37

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -8,8 +8,15 @@ function Profile() {
 
   return (
    <Container maxWidth="md">
-      <Box  sx={{ flexDirection:'column'}}><Avatar sx={{maxHeight:'md'}} id="avatar" src="/broken-image.jpg" /></Box>
-      <Box sx={{ flexDirection: 'column', bgcolor: '#e8e8e8', width: '135vh' , height: '100vh' }} />
+      <Box id = "avatarBox" sx={{ flexDirection:'column'}}><Avatar sx={{maxHeight:'md'}} id="avatar" src="/broken-image.jpg" /><h2>&nbsp;John Doe</h2></Box>
+      <Box sx={{ flexDirection: 'column', bgcolor: '#e8e8e8', width: '135vh' , height: '100vh' }}>
+        <p id = "basicInfo">
+          <br></br>Age: 30
+          <br></br>Birthday: 1 July 1991
+          <br></br>Weight: 150 lb
+          <br></br>Address: 101 Brooke, Montreal L5L 9T9
+        </p>
+      </Box>
     </Container>
   );
 }
