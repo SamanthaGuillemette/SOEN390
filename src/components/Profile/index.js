3510dbbfb1efeb61f5a4954b931902dadfc9aff3
salehatrq <68137308+salehatrq@users.noreply.github.com>
2022-01-21 16:58:44 -0500

    Edited css for avatar and container #37

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -7,13 +7,10 @@ import Container from '@mui/material/Container';
 function Profile() {
 
   return (
-   <Container id="container" maxWidth="md">
-    
-    
-      <Box  sx={{ flexDirection:'column', maxHeight:3/4}}><Avatar sx={{maxHeight:'md'}} id="avatar" src="/broken-image.jpg" /></Box>
+   <Container maxWidth="md">
+      <Box  sx={{ flexDirection:'column'}}><Avatar sx={{maxHeight:'md'}} id="avatar" src="/broken-image.jpg" /></Box>
       <Box sx={{ flexDirection: 'column', bgcolor: '#e8e8e8', width: '135vh' , height: '100vh' }} />
     </Container>
-  
   );
 }
 
