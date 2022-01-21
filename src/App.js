fb4f17de3f347ad62d2537ed96bc37736158468b
Samantha Guillemette <samantha.guillemette@gmail.com>
2022-01-21 15:08:38 -0500

    continue merge main into Profile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
+
 import Dashboard from "./components/Dashboard";
 import Profile from "./components/Profile";
-=======
-import Dashboard from "./screens/Dashboard";
->>>>>>> main
+
 import AppBody from "./components/AppBody";
 import Appointments from "./screens/Appointments";
 import Patients from "./screens/Patients";
@@ -16,16 +14,14 @@ function App() {
       <AppBody>
         <Routes>
           <Route path="/" element={<Dashboard />} />
-<<<<<<< HEAD
+
           <Route path="/profile" element={<Profile />} />
-          <Route path="/notification" element={<Notification />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-=======
+      
+
           <Route path="/appointments" element={<Appointments />} />
           <Route path="/patients" element={<Patients />} />
           <Route path="/inbox" element={<Inbox />} />
->>>>>>> main
+
         </Routes>
       </AppBody>
     </BrowserRouter>
