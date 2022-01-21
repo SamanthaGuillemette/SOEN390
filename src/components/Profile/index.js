9011e2510c2b6a403573f71cb4419e17eae8194b
salehatrq <68137308+salehatrq@users.noreply.github.com>
2022-01-21 15:52:12 -0500

    Added container #37

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,11 +1,21 @@
 import "./profile.css";
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 function Profile() {
 
   return (
+    <React.Fragment>
+    <CssBaseline />
     <Avatar id="avatar" src="/broken-image.jpg" />
+    <Container id="container" maxWidth="md">
+      <Box sx={{ bgcolor: '#e8e8e8', width: '135vh' , height: '100vh' }} />
+    </Container>
+  </React.Fragment>
   );
 }
 
