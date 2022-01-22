2640de8f4e53cbdecbf95d27205a9f9786c607aa
Tushar Raval <tushar.raval102@gmail.com>
2022-01-22 01:02:47 -0500

    Implemented Signin and Signup pages
    Co-authored-by: Laila Alhalabi <lailaalhalabi@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./screens/Dashboard";
 import AppBody from "./components/AppBody";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 import Appointments from "./screens/Appointments";
 import Patients from "./screens/Patients";
 import Inbox from "./screens/Inbox";
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppBody>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/appointments" element={<Appointments />} />
-          <Route path="/patients" element={<Patients />} />
-          <Route path="/inbox" element={<Inbox />} />
-        </Routes>
-      </AppBody>
-    </BrowserRouter>
+     <BrowserRouter>
+       <AppBody>
+         <Routes>
+           <Route path="/signin" element={<SignIn />} />
+           <Route path="/signup" element={<SignUp />} />
+           <Route path="/" element={<Dashboard />} />
+           <Route path="/appointments" element={<Appointments />} />
+           <Route path="/patients" element={<Patients />} />
+           <Route path="/inbox" element={<Inbox />} />
+         </Routes>
+       </AppBody>
+     </BrowserRouter>
   );
 }
 
