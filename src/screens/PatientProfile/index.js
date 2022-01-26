6b4fcbd7920ad91cc75afdcd046c52185417b7d3
salehatrq <68137308+salehatrq@users.noreply.github.com>
2022-01-26 02:11:30 -0500

    Added assigned doctor grid

--- src/screens/PatientProfile/index.js
+++ src/screens/PatientProfile/index.js
@@ -13,7 +13,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
+import { Link } from 'react-router-dom';
 
 
 
@@ -30,13 +30,13 @@ function PatientProfile() {
   return (
 
     
-   <Grid container spacing={2} maxWidth="lg" alignItems="flex-end">
+   <Grid container spacing={2} maxWidth="lg" alignItems='flex-end'>
      <Grid item xs={8} lg={4}>
       <Card >
         <CardActionArea>
           <Avatar  id="avatar" src="https://upload.wikimedia.org/wikipedia/commons/5/5a/John_Doe%2C_born_John_Nommensen_Duchac.jpg" />
           <CardContent>
-            <Typography className = "title" gutterBottom variant="h5" component="div">
+            <Typography className = "title" gutterBottom variant='button' fontSize='1.2rem' component="div">
              John Doe
             </Typography>
             <Typography className = "text" variant="body2" color="text.secondary">
@@ -49,60 +49,80 @@ function PatientProfile() {
         </CardActionArea>
       </Card>
     </Grid>
-    <Grid item xs={6} lg={6} >
-    <Card>
-       <CardActionArea >
-         <CardContent>
-         <Typography gutterBottom variant="h5" component="div">
-              Status
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              <br></br>Status: 
-              <a class="ui red label">Positive</a>
-            </Typography>
-         </CardContent>
-        </CardActionArea>
-      </Card>
+
+    <Grid container spacing={2} item rowSpacing={2} direction='column' xs={6}>
+      <Grid item >
+      <Card>
+        <CardActionArea >
+          <CardContent>
+          <Typography gutterBottom variant='button' component="div">
+                Status
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                <br></br>Status: 
+                <a class="ui red label">Positive</a>
+              </Typography>
+           </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+    
+      <Grid item >
+      <Card>
+        <CardActionArea >
+          <CardContent>
+          <Typography gutterBottom variant='button' component="div">
+                Assigned Doctor
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                <div>
+                  <br></br>Name: 
+				          <Link to='/doctor'> Michael Scott</Link>
+		            </div>
+              </Typography>
+           </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
     </Grid>
+
     <Grid item xs={12} lg={10}>
-  
-  <TableContainer component={Paper}><h3>&nbsp;&nbsp;&nbsp;&nbsp;Details List</h3>
-      <Table sx={{ minWidth: 650}} aria-label="collapsable table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell align="right">Fever</TableCell>
-            <TableCell align="right">Cough</TableCell>
-            <TableCell align="right">Runny Nose</TableCell>
-            <TableCell align="right">Muscle Ache</TableCell>
-            <TableCell align="right">Tiredness</TableCell>
-            <TableCell align="right">Smell Loss</TableCell>
-            <TableCell align="right">Taste Loss</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.Date}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.Date}
-              </TableCell>
-              <TableCell align="right">{row.Fever}</TableCell>
-              <TableCell align="right">{row.Cough}</TableCell>
-              <TableCell align="right">{row.RunnyNose}</TableCell>
-              <TableCell align="right">{row.MuscleAche}</TableCell>
-              <TableCell align="right">{row.Tiredness}</TableCell>
-              <TableCell align="right">{row.SmellLoss}</TableCell>
-              <TableCell align="right">{row.TasteLoss}</TableCell>
-              
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-</Grid>
+      <TableContainer component={Paper}><h5><br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;DETAILS LIST</h5>
+          <Table sx={{ minWidth: 650}} aria-label="collapsable table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Date</TableCell>
+                <TableCell align="right">Fever</TableCell>
+                <TableCell align="right">Cough</TableCell>
+                <TableCell align="right">Runny Nose</TableCell>
+                <TableCell align="right">Muscle Ache</TableCell>
+                <TableCell align="right">Tiredness</TableCell>
+                <TableCell align="right">Smell Loss</TableCell>
+                <TableCell align="right">Taste Loss</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.Date}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.Date}
+                  </TableCell>
+                  <TableCell align="right">{row.Fever}</TableCell>
+                  <TableCell align="right">{row.Cough}</TableCell>
+                  <TableCell align="right">{row.RunnyNose}</TableCell>
+                  <TableCell align="right">{row.MuscleAche}</TableCell>
+                  <TableCell align="right">{row.Tiredness}</TableCell>
+                  <TableCell align="right">{row.SmellLoss}</TableCell>
+                  <TableCell align="right">{row.TasteLoss}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Grid>
 
     <Grid item xs={6} lg={4} >
       <Card>
