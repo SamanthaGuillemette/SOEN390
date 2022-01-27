51f0629ba62d5603330b9841ec99dbaa0e41e869
AliZ786 <59709752+AliZ786@users.noreply.github.com>
2022-01-27 03:40:51 -0500

    Placed cards properly

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -20,7 +20,7 @@ import ListSubheader from '@mui/material/ListSubheader';
 const Dashboard = () => {
   return (
     <Container maxWidth="xl">
-      <Grid container spacing={3} className="statContainer">
+      <Grid container spacing={2} className="statContainer">
         <Grid item xs={12} md={8}>
           <div className="statItem">
             <LineChart />
@@ -50,7 +50,7 @@ const Dashboard = () => {
       }}
      
       subheader={<li />} > 
-      <Typography className = "listTitle"  gutterBottom variant="h1" component="div">
+      <Typography className = "listTitle"  gutterBottom variant="h5">
       Patient's list
       </Typography>
       {[0, 1, 2, 3, 4].map((sectionId) => (
@@ -66,29 +66,68 @@ const Dashboard = () => {
         </li>
       ))}
     </List>
+   
+ 
 
-    <Card className="cardShape">
+   <Grid className="infoGrid" container spacing={2}>
+   <Typography className="title" gutterBottom variant="h5">
+   Important Links
+   </Typography>
+    <Card className="cardShape1">
     <CardMedia
       component="img"
-      height="140"
+      height="220"
       image="https://api.time.com/wp-content/uploads/2020/08/coronavirus-testing.jpg"
       alt="covid testing"
     />
     <CardContent>
     <p className = "card-details">Info</p>
-      <Typography className="cardTitle" gutterBottom variant="h5" component="div">
+      <Typography className="cardTitle" gutterBottom variant="h5" component="div"><a target="_blank" href="https://www.ciussswestcentral.ca/health-alerts/coronavirus-covid-19/covid-19-testing-clinics/">
         Covid-19 Testing Policies
+        </a>
       </Typography>
       <Typography className = "textContent" variant="body2" color="text.secondary">
         Find out how you could get tested if you develop any symptoms to the virus.
       </Typography>
     </CardContent>
-    <CardActions>
-      <Button size="small"> <a target="_blank" href="https://www.ciussswestcentral.ca/health-alerts/coronavirus-covid-19/covid-19-testing-clinics/">Learn More</a></Button>
-    </CardActions>
   </Card>
+
+  <Card className="cardShape2">
+  <CardMedia
+    component="img"
+    height="220"
+    image="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Flag_of_Quebec.svg/1200px-Flag_of_Quebec.svg.png"
+    alt="covid testing"
+  />
+  <CardContent>
+  <p className = "card-details">Info</p>
+    <Typography className="cardTitle" gutterBottom variant="h5" component="div">
+    <a target="_blank" href="https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/situation-coronavirus-in-quebec">Data of COVID-19 in Quebec</a>
+    </Typography>
+    <Typography className = "textContent" variant="body2" color="text.secondary">
+    Most people who fall sick with COVID-19 will experience mild to moderate symptoms and recover without special treatment. However, some will become seriously ill and require medical attention.
+    </Typography>
+  </CardContent>
+</Card>
       
-       
+<Card className="cardShape3">
+<CardMedia
+  component="img"
+  height="220"
+  image="https://cdn.ciusssnordmtl.ca/documents/Nouvelles/2021/MUSC_VaccinationCovid19_Nouvelle_site_webF.png?1614089135"
+  alt="covid testing"
+/>
+<CardContent>
+<p className = "card-details">Info</p>
+  <Typography className="cardTitle" gutterBottom variant="h5" component="div">
+  <a target="_blank" href="https://portal3.clicsante.ca/">Appointment for vaccination</a>
+  </Typography>
+  <Typography className = "textContent" variant="body2" color="text.secondary">
+    To book a appointment for the vaccination, as well as check eligibilty, click on this link for more info.
+  </Typography>
+</CardContent>
+</Card>
+      </Grid>
       </Grid>
     </Container>
   );
