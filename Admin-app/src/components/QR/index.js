920b2c6e4ac41357e7c1a4b2d2790121275cee21
Laila Alhalabi <laila.alhalabi99@gmail.com>
2022-02-17 12:39:47 -0500

    change library

--- Admin-app/src/components/QR/index.js
+++ Admin-app/src/components/QR/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {QrReader} from "react-qr-reader";
+import QrReader from 'modern-react-qr-reader';
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -18,8 +18,13 @@ const Scanner = () => {
   const [notPatient, setNotPatient] = useState(false);
   const [qr, setQR] = useState("");
 
+  function handleError (err) {
+    console.error(err)
+  } 
+
   async function handleScan(QRScan) {
     if (QRScan) {
+      console.log(QRScan);
       setPatient(false);
       setNotPatient(false);
       setDisplay(false);
@@ -50,9 +55,18 @@ const Scanner = () => {
             Scan a QR Code to Generate User's Profile
           </Typography>
           <QrReader
+            onResult={(result, error) => {
+              if (!!result) {
+                setScanned(result?.text);
+              }
+              if (!!error) {
+                console.info(error);
+              }
+            }}
+            delay={300}
+            onError={handleError}
             onScan={handleScan}
-            onError={(err) => console.log(err)}
-            style={{ width: "300px" }}
+            style={{ width: '300px' }}
           />
           <br />
           <p>{scanned}</p>
@@ -77,88 +91,3 @@ const Scanner = () => {
   );
 }
 export default Scanner;
-
-
-////////////////////////////////////////////////////////////////////////
-//////////////SHOULD BE MOVED TO THE CLIENT SIDE////////////////////////
-////////////////////////////////////////////////////////////////////////
-// // import { useAuthState } from "react-firebase-hooks/auth";
-// import { useDocumentOnce } from "react-firebase-hooks/firestore";
-// import { auth, db } from "../../backend/firebase";
-// import { doc } from "firebase/firestore";
-// import Button from "@mui/material/Button";
-// import Card from "@mui/material/Card";
-// import CardContent from "@mui/material/CardContent";
-// import Typography from "@mui/material/Typography";
-// import Box from "@mui/material/Box";
-
-// const QR = () => {
-//   // const [user, loading] = useAuthState(auth);
-//   const user = auth.currentUser;
-//   const [value] = useDocumentOnce(doc(db, "Users", user?.email));
-//   const qrCode = `http://api.qrserver.com/v1/create-qr-code/?data=${user?.email}`;
-
-//   return (
-//     <>
-//       <Card sx={{ m: "30px" }}>
-//         <CardContent
-//           sx={{
-//             display: "flex",
-//             flexDirection: "column",
-//             alignItems: "center",
-//             p: "30px",
-//             mb: "20px",
-//           }}
-//         >
-//           <Typography
-//             sx={{
-//               mx: "30px",
-//               pb: "30px",
-//               textAlign: "center",
-//               fontWeight: "800",
-//             }}
-//             color="text.secondary"
-//             variant="h5"
-//           >
-//             Patient Details
-//           </Typography>
-//           <Typography
-//             sx={{ mx: "360px", justifyContent: "center", fontSize: 14 }}
-//             variant="body2"
-//           >
-//             <strong>First Name: </strong>
-//             {value && value.data().firstName}
-//             {/* {loading && "loading..."} */}
-//             <br />
-//             <br />
-//             <strong>Last Name: </strong>
-//             {/* {loading && "loading..." */}
-//             {value && value.data().lastName}
-//             <br />
-//             <br />
-//             <strong>Email: </strong>
-//             {value && value.data().email}
-//             <br />
-//             <br />
-//           </Typography>
-
-//           <Box sx={{ mx: "350px", mb: "30px" }}>
-//             <img alt="QRCode" src={qrCode} />
-//           </Box>
-
-//           <Button
-//             href={qrCode}
-//             target="_blank"
-//             variant="contained"
-//             color="success"
-//             sx={{ ":hover": { color: "white" } }}
-//           >
-//             Download QR Code
-//           </Button>
-//         </CardContent>
-//       </Card>
-//     </>
-//   );
-// };
-
-// export default QR;
