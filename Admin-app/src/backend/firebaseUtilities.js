4dbe95d0ff9f81cbf1b6b3f8ac1272d1ea0e486a
smarkandu <s_markan@hotmail.com>
2022-02-19 15:38:37 -0500

    Try/Catch blocks added for all fns in firebaseUtilities

--- Admin-app/src/backend/firebaseUtilities.js
+++ Admin-app/src/backend/firebaseUtilities.js
@@ -4,37 +4,58 @@ import patientData from "../data/patients.json";
 import { db } from "./firebase";
 
 const getPatients = async () => {
+  try
+  {
     console.log("getPatients Called");
     const querySnapshot = await getDocs(collection(db, "Patients"));
     const returnValue = querySnapshot.docs.map((patient) => patient.data());
     return returnValue;
-  };
+  }
+  catch(error)
+  {
+    console.log("[getPatients]" + error);  
+  }
+};
   
   const getPatient = async (id) => {
-    // Do call to firebase
-    const docRef = doc(db, "Patients", id);
-    const docSnapShot = await getDoc(docRef);
-    
-    // If file exists, return it
-    if (docSnapShot.exists()) {
-      console.log("Patient document Found!");
-      return docSnapShot.data();
-    } 
-    else {
-      // If not found, write to console.
-      console.log("Patient document not found");
+    try
+    {
+      // Do call to firebase
+      const docRef = doc(db, "Patients", id);
+      const docSnapShot = await getDoc(docRef);
+      
+      // If file exists, return it
+      if (docSnapShot.exists()) {
+        console.log("Patient document Found!");
+        return docSnapShot.data();
+      } 
+      else {
+        // If not found, write to console.
+        console.log("Patient document not found");
+      }
     }
-    };
+    catch(error)
+    {
+      console.log("[getPatient]" + error);  
+    }
+  };
   
   /**
    * This function populates the Patient table in firebase given a JSON file
    * imported at the beginning of this file
    */  
   const populatePatients = () => {
-    const patientsRef = collection(db, "Patients");
+    try
+    {
+      const patientsRef = collection(db, "Patients");
   
-    patientData.map((patientData) =>
-      setDoc(doc(patientsRef, patientData.id), patientData));
+      patientData.map((patientData) =>
+        setDoc(doc(patientsRef, patientData.id), patientData));
+    }
+    catch(error)
+    {
+      console.log("[populatePatients]" + error);  
+    }
   }
 
   export { getPatients, getPatient, populatePatients };
