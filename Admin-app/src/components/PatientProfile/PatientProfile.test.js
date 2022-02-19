e11655f53d44f5eff9731c42f8db2ca5586f1a9f
Quang Tran <nhatquang304@gmail.com>
2022-02-19 13:27:52 -0500

    fixed PatientProfile layout issue & bug #127

--- Admin-app/src/components/PatientProfile/PatientProfile.test.js
+++ Admin-app/src/components/PatientProfile/PatientProfile.test.js
@@ -2,7 +2,7 @@ import { render, screen, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import PatientProfile from ".";
 
-test.skip("PatientProfile should render without errors", () => {
+test("PatientProfile should render without errors", () => {
   render(<PatientProfile />);
 });
 
--- Admin-app/src/components/PatientProfile/index.js
+++ Admin-app/src/components/PatientProfile/index.js
@@ -1,11 +1,10 @@
 import "./PatientProfile.css";
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
+import { Box, CardActionArea } from "@mui/material";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -19,11 +18,11 @@ import Stack from "@mui/material/Stack";
 import { styled } from "@mui/material/styles";
 import InputLabel from "@mui/material/InputLabel";
 import FormControl from "@mui/material/FormControl";
-import NativeSelect from '@mui/material/NativeSelect';
-import Checkbox from '@mui/material/Checkbox';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import Button from '@mui/material/Button';
-import FlagIcon from '@mui/icons-material/Flag';
+import NativeSelect from "@mui/material/NativeSelect";
+import Checkbox from "@mui/material/Checkbox";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import Button from "@mui/material/Button";
+import FlagIcon from "@mui/icons-material/Flag";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getPatient } from "../../backend/firebase";
@@ -35,8 +34,7 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
-function getAge(dobStr)
-{
+function getAge(dobStr) {
   // First get today's date
   var todaysDate = new Date();
 
@@ -47,22 +45,21 @@ function getAge(dobStr)
   var returnValue = todaysDate.getYear() - dob.getYear();
 
   // Check month in case it has an effect on the age
-  if (dob.getMonth() > todaysDate.getMonth())
-  {
+  if (dob.getMonth() > todaysDate.getMonth()) {
     returnValue += 1;
   }
   // If dob same month as today, check day in case it has an effect on the age
-  else if (dob.getMonth() === todaysDate.getMonth() && dob.getDay() > todaysDate.getDay())
-  {
+  else if (
+    dob.getMonth() === todaysDate.getMonth() &&
+    dob.getDay() > todaysDate.getDay()
+  ) {
     returnValue += 1;
   }
-
 
   return returnValue;
 }
 
 function PatientProfile() {
-  
   function createData(
     Date,
     Fever,
@@ -87,109 +84,152 @@ function PatientProfile() {
 
   const rows = [
     createData("Jan 25", "No", "Yes", "No", "Yes", "Yes", "No", "No"),
-    createData("Jan 26", "No", "Yes", "No", "No", "No", "No", "No")
+    createData("Jan 26", "No", "Yes", "No", "No", "No", "No", "No"),
   ];
 
   const [priorityFlag, setPriorityFlag] = useState(false);
   const { id } = useParams();
-  const [patientInfo, setPatientInfo] = useState(null); 
+  const [patientInfo, setPatientInfo] = useState(null);
 
   useEffect(() => {
-    const data = localStorage.getItem('priorityFlag');
-    if (data){
+    const data = localStorage.getItem("priorityFlag");
+    if (data) {
       setPriorityFlag(JSON.parse(data));
     }
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('priorityFlag', JSON.stringify(priorityFlag));
+    localStorage.setItem("priorityFlag", JSON.stringify(priorityFlag));
   });
 
   // Get Patient Info each time page refreshes
   useEffect(() => {
     //console.log("id" + id);
-    getPatient(id).then((data) => {
-      setPatientInfo(data); 
-    });
-  }, [id])
+    getPatient(id)
+      .then((data) => {
+        setPatientInfo(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [id]);
 
   return (
-    <Grid container spacing={2} maxWidth="lg" alignItems="flex-end">
-      <Grid item xs={8} lg={4}>
+    <Grid container spacing={2} maxWidth="lg" margin={2}>
+      <Grid item xs={12} lg={4}>
         <Card>
           <CardActionArea>
-            <Avatar
-              id="avatar"
-              src={patientInfo && patientInfo.profileImage}
-            />
+            <Box
+              sx={{ display: "flex", justifyContent: "center", marginTop: 3 }}
+            >
+              <img
+                alt={patientInfo ? patientInfo.name : "Patient"}
+                className="patientProfile-avatar"
+                src={patientInfo && patientInfo.profileImage}
+              />
+            </Box>
             <CardContent>
               <Typography
-                className="profile-name"
+                className="patientProfile-name"
                 gutterBottom
                 variant="button"
                 fontSize="1.2rem"
                 component="div"
               >
-              {patientInfo && patientInfo.name}
+                {patientInfo && patientInfo.name}
               </Typography>
-              <Typography
-                className="text"
-                variant="body2"
-                color="text.secondary"
-              >
-                <br></br>Age: {patientInfo && getAge(patientInfo.dob)}
-                <br></br>Birthday: {patientInfo && patientInfo.dob}
-                <br></br>{patientInfo && patientInfo.address}
-              </Typography>
+              <Box className="patientProfile-metaData">
+                <Typography className="patientProfile-metaData_text">
+                  Age:{patientInfo && getAge(patientInfo.dob)}
+                </Typography>
+                <Typography className="patientProfile-metaData_text">
+                  Birthday: {patientInfo && patientInfo.dob}
+                </Typography>
+                <Typography className="patientProfile-metaData_text">
+                  {patientInfo && patientInfo.address}
+                </Typography>
+              </Box>
             </CardContent>
           </CardActionArea>
         </Card>
       </Grid>
 
-      <Grid container spacing={2} item rowSpacing={2} direction="column" xs={6.1}>
+      <Grid
+        container
+        spacing={2}
+        item
+        rowSpacing={2}
+        direction="column"
+        xs={12}
+        lg={8}
+      >
         <Grid item>
-          <Card className={priorityFlag ? "status-card clicked" : "status-card"}>
+          <Card
+            className={priorityFlag ? "status-card clicked" : "status-card"}
+          >
             <CardActionArea>
               <CardContent>
-                <Typography gutterBottom variant="button" component="div">
-                  Status  <FlagIcon onClick={() => {priorityFlag ? setPriorityFlag(false) : setPriorityFlag(true)}}
-//                Commented for now SMD - Will eventually need to be reactivated in order to read value from DB     
-//                  className={patientInfo && patientInfo.flaggedPriority === "1" ? "priority-flag clicked" : "priority-flag"}>
-                  className={priorityFlag ? "priority-flag clicked" : "priority-flag"}>
-                  </FlagIcon>
-                  <br></br>
-                  <br></br>
+                <Typography
+                  gutterBottom
+                  variant="button"
+                  component="div"
+                  className="patientProfile-statusFlag"
+                >
+                  Status
+                  <FlagIcon
+                    onClick={() => {
+                      priorityFlag
+                        ? setPriorityFlag(false)
+                        : setPriorityFlag(true);
+                    }}
+                    //                Commented for now SMD - Will eventually need to be reactivated in order to read value from DB
+                    //                  className={patientInfo && patientInfo.flaggedPriority === "1" ? "priority-flag clicked" : "priority-flag"}>
+                    className={
+                      priorityFlag ? "priority-flag clicked" : "priority-flag"
+                    }
+                  ></FlagIcon>
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  <div>
-                    <Stack
-                      direction="row"
-                      divider={<Divider orientation="vertical" />}
-                      spacing={1}
-                      alignItems="baseline"
+                <Box>
+                  <Stack
+                    direction="row"
+                    divider={<Divider orientation="vertical" />}
+                    spacing={1}
+                    alignItems="baseline"
+                  >
+                    <FormControl sx={{ width: 115 }}>
+                      <InputLabel
+                        variant="standard"
+                        htmlFor="uncontrolled-native"
+                      >
+                        Confirmation
+                      </InputLabel>
+                      <NativeSelect
+                        size="small"
+                        defaultValue={20}
+                        inputProps={{
+                          name: "confirmation",
+                          id: "uncontrolled-native",
+                        }}
+                      >
+                        <option value={10}>Confirmed</option>
+                        <option value={20}>Unconfirmed</option>
+                      </NativeSelect>
+                    </FormControl>
+                    <Item
+                      className={
+                        patientInfo && patientInfo.status === "POSITIVE"
+                          ? "label-positive"
+                          : "label-negative"
+                      }
                     >
-                      
-                      <FormControl sx={{width: 115}} >
-                        <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                          Confirmation
-                        </InputLabel>
-                        <NativeSelect
-                          size="small"
-                          defaultValue={20}
-                          inputProps={{
-                            name: 'confirmation',
-                            id: 'uncontrolled-native',
-                          }}>
-                          <option value={10}>Confirmed</option>
-                          <option value={20}>Unconfirmed</option>
-                        </NativeSelect>
-                      </FormControl>
-                      <Item className={patientInfo && patientInfo.status === "POSITIVE"?"label-positive":"label-negative"}>{patientInfo && patientInfo.status}</Item>
-                      <Item>Temperature: {patientInfo && patientInfo.temperature} °C</Item>
-                      <Item>Weight: {patientInfo && patientInfo.weight} lbs</Item>
-                    </Stack>
-                  </div>
-                </Typography>
+                      {patientInfo && patientInfo.status}
+                    </Item>
+                    <Item>
+                      Temperature: {patientInfo && patientInfo.temperature} °C
+                    </Item>
+                    <Item>Weight: {patientInfo && patientInfo.weight} lbs</Item>
+                  </Stack>
+                </Box>
               </CardContent>
             </CardActionArea>
           </Card>
@@ -204,12 +244,10 @@ function PatientProfile() {
                     Assigned Doctor
                   </Typography>
                   <Typography variant="body2" color="text.secondary">
-                    Name:{" "}
-                    <Link to="/doctor" className="link">
-                      {" "}
-                      {patientInfo && patientInfo.assignedDoctor}
-                    </Link>
-                    <Checkbox size="small" style ={{color: "white"}}/>
+                    {/* Name: <Link to="/doctor" className="link">{" "} */}
+                    {patientInfo && patientInfo.assignedDoctor}
+                    {/* </Link> */}
+                    <Checkbox size="small" style={{ color: "white" }} />
                   </Typography>
                 </CardContent>
               </CardActionArea>
@@ -224,7 +262,7 @@ function PatientProfile() {
                     Status Review
                   </Typography>
                   <Typography variant="body2" color="text.secondary">
-                    Review Completed: <Checkbox size="small"/>
+                    Review Completed: <Checkbox size="small" />
                   </Typography>
                 </CardContent>
               </CardActionArea>
@@ -233,12 +271,14 @@ function PatientProfile() {
         </Grid>
       </Grid>
 
-      <Grid item xs={12} lg={10.1}>
+      <Grid item xs={12}>
         <TableContainer component={Paper}>
           <h5>
             <br />
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SYMPTOM DETAILS
-            <Button id="add-button"><AddCircleIcon></AddCircleIcon></Button>
+            <Button id="add-button">
+              <AddCircleIcon></AddCircleIcon>
+            </Button>
           </h5>
           <Table sx={{ minWidth: 650 }} aria-label="collapsable table">
             <TableHead>
