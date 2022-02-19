9aafc148edbc754e573c86bfa08a1f352b0ed975
smarkandu <s_markan@hotmail.com>
2022-02-19 17:21:10 -0500

    Modified to match master

--- Admin-app/src/components/PatientProfile/index.js
+++ Admin-app/src/components/PatientProfile/index.js
@@ -95,8 +95,8 @@ function PatientProfile() {
   const [patientInfo, setPatientInfo] = useState(null);
 
   useEffect(() => {
-    const data = localStorage.getItem("priorityFlag");
-    if (data) {
+    const data = localStorage.getItem('priorityFlag');
+    if (data){
       setPriorityFlag(JSON.parse(data));
     }
   }, []);
