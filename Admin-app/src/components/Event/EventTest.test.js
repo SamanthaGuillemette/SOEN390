829e3f4989b4b2b704b20e44c5839cf2fbb29a0a
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2022-02-20 11:54:12 -0500

    Added event details test

--- Admin-app/src/components/Event/EventTest.test.js
+++ Admin-app/src/components/Event/EventTest.test.js
@@ -1,14 +1,29 @@
 import { MemoryRouter } from "react-router-dom";
 import {render, screen, cleanup} from "@testing-library/react";
 import "@testing-library/jest-dom";
-import EventButton from "../EventButton/index"
+import EventButton from "../EventButton/index";
+import EventList  from "./EventList";
+import EventDetails from "./EventDetails";
 
 
 test("should render event button", () => {
-render(<MemoryRouter><EventButton /></MemoryRouter>)
+render(<MemoryRouter><EventButton /></MemoryRouter>);
 
 
 });
+
+// test("should render event list", () => {
+// render(<MemoryRouter><EventList /></MemoryRouter>);
+
+
+// });
+
+test("should render event details", () => {
+render(<MemoryRouter><EventDetails /></MemoryRouter>);
+
+});
+
+
 
 afterEach(() => {
     cleanup();
