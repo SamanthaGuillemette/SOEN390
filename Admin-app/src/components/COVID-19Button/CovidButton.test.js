5fea850e16523aa42b616686d6034c4cc847bb96
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2022-02-20 01:07:19 -0500

    Fixed covid button test

--- Admin-app/src/components/COVID-19Button/CovidButton.test.js
+++ Admin-app/src/components/COVID-19Button/CovidButton.test.js
@@ -1,12 +1,14 @@
 import { render, screen, cleanup } from "@testing-library/react";
+import {Router} from "@testing-library/react"
 import "@testing-library/jest-dom";
 import COVID19Button from "./index"
 
 test("should render covid button", () => {
+  <Router>
     render(<COVID19Button />);
     const covidbutton = screen.getByTestId("covid-1");
     expect(covidbutton).toBeInTheDocument();
-
+</Router>
   });
   
   afterEach(() => {
