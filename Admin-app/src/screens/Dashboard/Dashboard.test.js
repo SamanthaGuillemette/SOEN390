4e8febf25efa20f4003d8bb35581a41ba95b26f8
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2022-02-21 01:33:35 -0500

    Added test for card components

--- Admin-app/src/screens/Dashboard/Dashboard.test.js
+++ Admin-app/src/screens/Dashboard/Dashboard.test.js
@@ -3,6 +3,15 @@ import {render, screen, cleanup} from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Dashboard from "./index";
 
+
 test("should get the dashboard" ,() =>{
     render(<MemoryRouter><Dashboard/></MemoryRouter>);
-})
+    const card2 = screen.getByTestId("card2");
+    expect(card2).toBeInTheDocument();
+
+});
+
+afterEach(() =>{
+    cleanup();
+});
+
