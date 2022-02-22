5c2544c1afb0b1f4b41f6e539563f44c345de578
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2022-02-22 01:36:55 -0500

    Added specific assertions

--- Admin-app/cypress/integration/dashboard.spec.js
+++ Admin-app/cypress/integration/dashboard.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 
+// overall description of the test
 describe("Dashboard page testing", () => {
     it("first", () =>{
         cy.visit('/');
@@ -8,7 +9,14 @@ describe("Dashboard page testing", () => {
         cy.contains('Admin Dashboard');
         // cy.contains("Upcoming Events");
         cy.contains("Patient's list");
-        cy.contains("Blood Donations")
+        cy.contains("Blood Donations");
+
+        // Added asserts 
+        cy.get('h5').contains('Important Links');
+        cy.get('a').contains('Data of COVID-19 in Quebec');
+
+        // Added click assertions
+        cy.get('a').contains('Appointment for vaccination').click();
     })
 
 
