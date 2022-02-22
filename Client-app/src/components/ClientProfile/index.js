d60dcdf59a603d91487478828c1c95d4eb5e3e4f
Quang Tran <nhatquang304@gmail.com>
2022-02-21 21:42:36 -0500

    fixed weird z-index error in Client profile background

--- Client-app/src/components/ClientProfile/index.js
+++ Client-app/src/components/ClientProfile/index.js
@@ -41,114 +41,112 @@ function ClientProfile() {
   });
 
   return (
-    <Box className="clientProfile-container">
-      <Grid container spacing={3} padding={5}>
-        <Grid item xs={12}>
-          <Card>
-            <Box className="clientProfile-profileCard">
-              <Fab
-                color="primary"
-                aria-label="edit"
-                className="clientProfile-editIcon"
+    <Grid container spacing={3} padding={5}>
+      <Grid item xs={12}>
+        <Card>
+          <Box className="clientProfile-profileCard">
+            <Fab
+              color="primary"
+              aria-label="edit"
+              className="clientProfile-editIcon"
+            >
+              <EditIcon fontSize="small" />
+            </Fab>
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Avatar
+                id="clientProfile-avatar"
+                src="https://cdn.discordapp.com/attachments/943266123393142804/943303072300548156/Stevie.png"
+              />
+            </Grid>
+            <CardContent>
+              <Typography
+                className="profile-name"
+                gutterBottom
+                variant="button"
+                fontSize="1.2rem"
+                component="div"
               >
-                <EditIcon fontSize="small" />
-              </Fab>
-              <Grid
-                container
-                direction="column"
-                justifyContent="center"
-                alignItems="center"
-              >
-                <Avatar
-                  id="clientProfile-avatar"
-                  src="https://cdn.discordapp.com/attachments/943266123393142804/943303072300548156/Stevie.png"
-                />
-              </Grid>
+                Jane Doe
+              </Typography>
+              <Box className="clientProfile-text">
+                <p className="clientProfile-textDetail">Age: 50</p>
+                <p className="clientProfile-textDetail">
+                  Birthday: 1 July 1971
+                </p>
+                <p className="clientProfile-textDetail">
+                  Address: 101 Brooke, Montreal L5L 9T9
+                </p>
+              </Box>
+            </CardContent>
+          </Box>
+        </Card>
+      </Grid>
+      <Grid item xs={12}>
+        <Grid item>
+          <Card
+            className={priorityFlag ? "status-card clicked" : "status-card"}
+          >
+            <CardActionArea>
               <CardContent>
-                <Typography
-                  className="profile-name"
-                  gutterBottom
-                  variant="button"
-                  fontSize="1.2rem"
-                  component="div"
-                >
-                  Jane Doe
-                </Typography>
-                <Box className="clientProfile-text">
-                  <p className="clientProfile-textDetail">Age: 50</p>
-                  <p className="clientProfile-textDetail">
-                    Birthday: 1 July 1971
-                  </p>
-                  <p className="clientProfile-textDetail">
-                    Address: 101 Brooke, Montreal L5L 9T9
-                  </p>
+                <Box className="clientProfile-statusBox">
+                  Status
+                  <FlagIcon
+                    onClick={() => {
+                      priorityFlag
+                        ? setPriorityFlag(false)
+                        : setPriorityFlag(true);
+                    }}
+                    className={
+                      priorityFlag
+                        ? "clientProfile-priority-flag clicked"
+                        : "clientProfile-priority-flag"
+                    }
+                  ></FlagIcon>
                 </Box>
+                <Stack
+                  direction="row"
+                  divider={<Divider orientation="vertical" />}
+                  spacing={1}
+                  marginBottom={2}
+                  alignItems="baseline"
+                >
+                  <FormControl sx={{ width: 115 }}>
+                    <InputLabel
+                      variant="standard"
+                      htmlFor="uncontrolled-native"
+                    >
+                      Confirmation
+                    </InputLabel>
+                    <NativeSelect
+                      size="small"
+                      defaultValue={20}
+                      inputProps={{
+                        name: "confirmation",
+                        id: "uncontrolled-native",
+                      }}
+                    >
+                      <option value={10}>Confirmed</option>
+                      <option value={20}>Unconfirmed</option>
+                    </NativeSelect>
+                  </FormControl>
+                  <span className="label-positive">positive</span>
+                </Stack>
+                <Box />
+                <Stack spacing={2}>
+                  <Item>Temperature: 39 °C</Item>
+                  <Item>Weight: 150 lbs</Item>
+                </Stack>
               </CardContent>
-            </Box>
+            </CardActionArea>
           </Card>
         </Grid>
-        <Grid item xs={12}>
-          <Grid item>
-            <Card
-              className={priorityFlag ? "status-card clicked" : "status-card"}
-            >
-              <CardActionArea>
-                <CardContent>
-                  <Box className="clientProfile-statusBox">
-                    Status
-                    <FlagIcon
-                      onClick={() => {
-                        priorityFlag
-                          ? setPriorityFlag(false)
-                          : setPriorityFlag(true);
-                      }}
-                      className={
-                        priorityFlag
-                          ? "clientProfile-priority-flag clicked"
-                          : "clientProfile-priority-flag"
-                      }
-                    ></FlagIcon>
-                  </Box>
-                  <Stack
-                    direction="row"
-                    divider={<Divider orientation="vertical" />}
-                    spacing={1}
-                    marginBottom={2}
-                    alignItems="baseline"
-                  >
-                    <FormControl sx={{ width: 115 }}>
-                      <InputLabel
-                        variant="standard"
-                        htmlFor="uncontrolled-native"
-                      >
-                        Confirmation
-                      </InputLabel>
-                      <NativeSelect
-                        size="small"
-                        defaultValue={20}
-                        inputProps={{
-                          name: "confirmation",
-                          id: "uncontrolled-native",
-                        }}
-                      >
-                        <option value={10}>Confirmed</option>
-                        <option value={20}>Unconfirmed</option>
-                      </NativeSelect>
-                    </FormControl>
-                    <span className="label-positive">positive</span>
-                  </Stack>
-                  <Box />
-                  <Stack spacing={2}>
-                    <Item>Temperature: 39 °C</Item>
-                    <Item>Weight: 150 lbs</Item>
-                  </Stack>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        </Grid>
       </Grid>
-    </Box>
+    </Grid>
   );
 }
 
