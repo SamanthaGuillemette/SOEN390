ebbd9427d1e172780184b364d35f1d2b05ef00bf
Marwa Khalid <khalidmarwa786@gmail.com>
2022-02-22 15:07:24 -0500

    Added dropdown + dashboard navigation

--- Client-app/src/components/BottomNav/index.js
+++ Client-app/src/components/BottomNav/index.js
@@ -5,13 +5,29 @@ import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
 import MailRoundedIcon from "@mui/icons-material/MailRounded";
 import MoreHorizRoundedIcon from "@mui/icons-material/MoreHorizRounded";
 import { Link } from "react-router-dom";
+import React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 const BottomNav = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+
   return (
     <div className="bottomNav-container">
       <IconButton size="large" className="bottomNav-button">
+      <Link to="../">
         <HomeRoundedIcon className="bottomNav-icon" sx={{ color: "white" }} />
         <p className="bottomNav-iconTitle">Home</p>
+        </Link>
       </IconButton>
       <Link className="client-profile" to="../ClientProfile">
         <IconButton size="large" className="bottomNav-button">
@@ -23,10 +39,28 @@ const BottomNav = () => {
         <MailRoundedIcon className="bottomNav-icon" sx={{ color: "white" }} />
         <p className="bottomNav-iconTitle">Inbox</p>
       </IconButton>
-      <IconButton size="large" className="bottomNav-button">
-        <MoreHorizRoundedIcon className="bottomNav-icon" sx={{ color: "white" }} />
-        <p className="bottomNav-iconTitle">More</p>
-      </IconButton>
+        <Button
+            id="basic-button"
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+            className="bottomNav-button"
+          >
+            <MoreHorizRoundedIcon className="bottomNav-icon" sx={{ color: "white" }} />
+            More
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+          >
+            <MenuItem className="more-dropdown" onClick={handleClose}>Logout</MenuItem>
+          </Menu>
     </div>
   );
 };
