145a830025dcec6031605e79cebe8189d8c359d2
Marwa Khalid <khalidmarwa786@gmail.com>
2022-02-22 16:44:46 -0500

    Cleaned code

--- Client-app/src/components/SymptomsTable/index.js
+++ Client-app/src/components/SymptomsTable/index.js
@@ -1,45 +1,13 @@
 import * as React from "react";
-import { useTheme } from "@mui/material/styles";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableFooter from "@mui/material/TableFooter";
-import TablePagination from "@mui/material/TablePagination";
 import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { makeStyles } from "@material-ui/core/styles";
-import { CardHeader } from "@mui/material";
-import Avatar from "@material-ui/core/Avatar";
-import HealingIcon from "@mui/icons-material/Healing";
-import IconButton from "@mui/material/IconButton";
-import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
-import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
-import VirusIcon from "../../assets/virus.svg";
+import TableRow from "@mui/material/TableRow";import VirusIcon from "../../assets/virus.svg";
 import "./SymptomsTable.css";
-import { padding } from "@mui/system";
-
-const dropdownStyle = makeStyles({
-  paper: {
-    background: "var(--background-main)",
-    color: "var(--text-inactive)",
-    borderRadius: "10px",
-  },
-  color: {
-    color: "var(--text-inactive)",
-  },
-  select: {
-    "&:after": {
-      borderBottomColor: "var(--text-inactive)",
-    },
-    "& .MuiSvgIcon-root": {
-      color: "var(--text-inactive)",
-    },
-  },
-});
 
 function createData(symptomDate, status) {
   return { symptomDate, status };
@@ -50,59 +18,7 @@ const rows = [
   createData("22/03/22", <span className="label-positive">positive</span>),
 ];
 
-function TablePaginationActions(props) {
-  const theme = useTheme();
-  const { count, page, rowsPerPage, onPageChange } = props;
-
-  const handleBackButtonClick = (event) => {
-    onPageChange(event, page - 1);
-  };
-
-  const handleNextButtonClick = (event) => {
-    onPageChange(event, page + 1);
-  };
-
-  return (
-    <div sx={{ flexShrink: 0, ml: 2.5 }}>
-      <IconButton
-        onClick={handleBackButtonClick}
-        disabled={page === 0}
-        aria-label="previous page"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowRight />
-        ) : (
-          <KeyboardArrowLeft />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="next page"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowLeft />
-        ) : (
-          <KeyboardArrowRight />
-        )}
-      </IconButton>
-    </div>
-  );
-}
-
 function SymptomsTable() {
-  const classes = dropdownStyle();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
   return (
     <TableContainer className="symptoms-list">
       <Box className="label">
@@ -128,10 +44,7 @@ function SymptomsTable() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {(rowsPerPage > 0
-            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            : rows
-          ).map((row) => (
+          {rows.map((row) => (
             <TableRow key={row.name}>
               <TableCell
                 className="data"
