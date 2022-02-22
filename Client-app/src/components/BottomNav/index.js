bec66097e194050caa26a4cf5c9d3138799752ce
Samantha Guillemette <samantha.guillemette@gmail.com>
2022-02-22 16:55:07 -0500

    sign out button styled and routed

--- Client-app/src/components/BottomNav/index.js
+++ Client-app/src/components/BottomNav/index.js
@@ -5,13 +5,17 @@ import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
 import MailRoundedIcon from "@mui/icons-material/MailRounded";
 import MoreHorizRoundedIcon from "@mui/icons-material/MoreHorizRounded";
 import { Link } from "react-router-dom";
-import React from 'react';
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
+import React from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useState } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../../backend/firebase";
 
 const BottomNav = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -20,18 +24,30 @@ const BottomNav = () => {
     setAnchorEl(null);
   };
 
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+  const logout = async (e) => {
+    e.preventDefault();
+    signOut(auth);
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
 
   return (
     <div className="bottomNav-container">
       <IconButton size="large" className="bottomNav-button">
-      <Link to="../">
-        <HomeRoundedIcon className="bottomNav-icon" sx={{ color: "white" }} />
-        <p className="bottomNav-iconTitle">Home</p>
+        <Link to="../">
+          <HomeRoundedIcon className="bottomNav-icon" sx={{ color: "white" }} />
+          <p className="bottomNav-iconTitle">Home</p>
         </Link>
       </IconButton>
       <Link className="client-profile" to="../ClientProfile">
         <IconButton size="large" className="bottomNav-button">
-          <AccountCircleRoundedIcon className="bottomNav-icon" sx={{ color: "white" }} />
+          <AccountCircleRoundedIcon
+            className="bottomNav-icon"
+            sx={{ color: "white" }}
+          />
           <p className="bottomNav-iconTitle">Profile</p>
         </IconButton>
       </Link>
@@ -39,28 +55,47 @@ const BottomNav = () => {
         <MailRoundedIcon className="bottomNav-icon" sx={{ color: "white" }} />
         <p className="bottomNav-iconTitle">Inbox</p>
       </IconButton>
-        <Button
-            id="basic-button"
-            aria-controls={open ? 'basic-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            onClick={handleClick}
-            className="bottomNav-button"
-          >
-            <MoreHorizRoundedIcon className="bottomNav-icon" sx={{ color: "white" }} />
-            More
-          </Button>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              'aria-labelledby': 'basic-button',
-            }}
-          >
-            <MenuItem className="more-dropdown" onClick={handleClose}>Logout</MenuItem>
-          </Menu>
+      <Button
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+        className="bottomNav-button"
+      >
+        <MoreHorizRoundedIcon
+          className="bottomNav-icon"
+          sx={{ color: "white" }}
+        />
+        More
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorReference="anchorPosition"
+        anchorPosition={{ top: 842, left: 638 }}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem
+          className="more-dropdown"
+          onMouseEnter={(e) => (e.target.style.color = "var(--text-primary)")}
+          onMouseLeave={(e) => (e.target.style.color = "var(--text-inactive)")}
+          onClick={logout}
+        >
+          Signout
+        </MenuItem>
+      </Menu>
     </div>
   );
 };
