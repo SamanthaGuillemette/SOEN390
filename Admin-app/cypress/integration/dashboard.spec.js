513239e209ecee46910385517e5a3d107e9dca12
AliZ786 <59709752+AliZ786@users.noreply.github.com>
2022-02-22 19:13:00 -0500

    Added some comments

--- Admin-app/cypress/integration/dashboard.spec.js
+++ Admin-app/cypress/integration/dashboard.spec.js
@@ -7,7 +7,6 @@ describe("Dashboard page testing", () => {
 
         // Added tests for the contains
         cy.contains('Admin Dashboard');
-        // cy.contains("Upcoming Events");
         cy.contains("Patient's list");
         cy.contains("Blood Donations");
 
