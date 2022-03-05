3bb59e1e86b623def1b52d587001988e14ce1388
Hoda Nourbakhsh <hodanourbakhsh@Hodas-MacBook-Pro.local>
2022-03-05 16:19:03 -0500

    Created the doctor's info page on the client app and created the proper route.

---
+++ Client-app/src/components/DoctorInfo/index.js
@@ -0,0 +1,113 @@
+import "./DoctorInfo.css";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import Divider from "@mui/material/Divider";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(0.5),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
+function DoctorInfo() {
+  return (
+    <Box className="doctorInfo-container">
+      <Grid container spacing={3} padding={5}>
+        <Grid item xs={12}>
+          <Card>
+            <Box className="doctorInfo-Card">
+              <Grid
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Avatar
+                  id="doctorInfo-avatar"
+                  src="https://media.discordapp.net/attachments/948246278490456064/949745262367232100/unknown.png"
+                />
+              </Grid>
+              <CardContent>
+                <Typography
+                  data-testid="avatar"
+                  className="name"
+                  gutterBottom
+                  variant="button"
+                  fontSize="1.2rem"
+                  component="div"
+                >
+                  Jamal Doe
+                </Typography>
+                <Box className="doctorInfo-text">
+                  <p className="doctorInfo-textDetail">Age: 86</p>
+                  <p className="doctorInfo-textDetail">Gender: male</p>
+                  <p className="doctorInfo-textDetail">Work Place: MUHC</p>
+                </Box>
+              </CardContent>
+            </Box>
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid item>
+            <Card className="info-card">
+              <CardActionArea>
+                <CardContent>
+                  <div>
+                    <Typography
+                      className="header"
+                      gutterBottom
+                      variant="button"
+                      component="div"
+                    >
+                      Info
+                    </Typography>
+                  </div>
+                  <Stack
+                    direction="row"
+                    divider={<Divider orientation="vertical" />}
+                    spacing={1}
+                    marginBottom={2}
+                    alignItems="baseline"
+                  ></Stack>
+                  <Box />
+                  <Stack spacing={2}>
+                    <Item
+                      className="info-data"
+                      sx={{ bgcolor: "black", boxShadow: "none" }}
+                    >
+                      Speciality: Family Doctor
+                    </Item>
+                    <Item
+                      className="info-data"
+                      sx={{ bgcolor: "black", boxShadow: "none" }}
+                    >
+                      Patient spots: 10/20
+                    </Item>
+                    <Item
+                      className="info-data"
+                      sx={{ bgcolor: "black", boxShadow: "none" }}
+                    >
+                      Experience: 7 years
+                    </Item>
+                  </Stack>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+export default DoctorInfo;
