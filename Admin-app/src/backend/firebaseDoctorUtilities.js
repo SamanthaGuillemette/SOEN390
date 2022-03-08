1861a397cf649bbb508ecf13c74e87f8bc26ae83
smarkandu <s_markan@hotmail.com>
2022-03-07 20:21:35 -0500

    Functions added to firebaseDoctorUtilities (not completed)

--- Admin-app/src/backend/firebaseDoctorUtilities.js
+++ Admin-app/src/backend/firebaseDoctorUtilities.js
@@ -1,5 +1,8 @@
 import doctorData from "../data/doctors.json";
 import { getTableData, getTableDataItem, populateTable } from "./firebaseUtilities";
+import { getPatients } from "./firebasePatientUtilities";
+import { doc, updateDoc, deleteField } from "firebase/firestore";
+import { db } from "./firebase";
 
 const tableName = "Doctors";
 const patientLimit = 10;
@@ -13,6 +16,52 @@ const getDoctor = async (id) => {
     return getTableDataItem(tableName, id);
 };
 
+const setTreats = async (doctorId, patientId) => {
+  try 
+  {
+    // Get Patient
+    const docRef = doc(db, tableName, doctorId);
+    let doctorInfo = await getDoctor(doctorId);
+
+    // Get All Patients
+    let patients = [];
+    getPatients().then((data) => {
+      data.forEach((doc) => {
+        patients[doc.id] = doc;
+      });
+    });
+
+    let arrayOfPatients = doctorInfo.treats;
+    docRef && await updateDoc(docRef, "treats", arrayOfPatients);
+
+    if (arrayOfPatients != null)
+    {
+      // Make sure patientId already isn't in treat table and is a valid patient Id
+      if (!(patientId in arrayOfPatients) && (patientId in patients))
+      {
+      // Update Assigned Doctor field in Patient
+      arrayOfPatients.add(patientId);
+      docRef && await updateDoc(docRef, "treats", arrayOfPatients);
+      }
+    }
+    else
+    {
+      // Delete Assigned Doctor field in Patient
+      docRef && await updateDoc(docRef, { "treats": deleteField() });
+    }
+
+    // Get updated patient
+    doctorInfo = await getDoctor(doctorId);      
+
+    return doctorInfo;
+  }
+  catch (error)
+  {
+    console.log("[setTreats]" + error);  
+  }
+};
+
+
 /**
  * This function populates the Patient table in firebase given a JSON file
  * imported at the beginning of this file
@@ -21,4 +70,4 @@ const getDoctor = async (id) => {
     populateTable(tableName, doctorData);
   }
 
-export { getDoctors, getDoctor, populateDoctors, patientLimit }
+export { getDoctors, getDoctor, populateDoctors, patientLimit, setTreats }
