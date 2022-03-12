7a2a64725cb7b40cf8bdfabd9cf66ff8e054a172
Quang Tran <nhatquang304@gmail.com>
2022-03-12 17:55:44 -0500

    updated client's appointment layout with better spacing

--- Client-app/src/screens/Appointment/index.js
+++ Client-app/src/screens/Appointment/index.js
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import "./Appointment.css";
 import { Button } from "@mui/material";
+import DoctorIcon from "../../assets/doctor-icon.svg";
 
 const theme = createTheme({
   palette: {
@@ -24,44 +25,46 @@ const Appointment = () => {
           <MenuAppBar />
 
           <div className="appointment-topContainer">
-            <div className="appointment-topTitle">Upcoming Appointment</div>
+            <h1 className="appointment-topTitle">Upcoming Appointment</h1>
             <div className="appointment-quickAccessContainer">
               <div className="appointment-quickAccess_DoctorIcon">
-                Doctor Icon
+                <img src={DoctorIcon} alt="Doctor Icon" width="50" />
               </div>
               <div className="appointment-quickAccess_InboxButton">
-                Inbox Button
+                <Button type="submit" variant="contained">
+                  Talk to doctor
+                </Button>
               </div>
             </div>
           </div>
 
           <div className="appointment-mainContainer">
-            <div className="appointment-title">
-              Meeting with Dr. Faye Anthony
-            </div>
-            <div className="appointment-description">Description:</div>
-            <div className="appointment-descriptionDetails">
+            <h2 className="appointment-title">Meeting with Dr. Faye Anthony</h2>
+            <h4 className="appointment-subTitle">Description:</h4>
+            <div className="appointment-textContent">
               Covid PCR testing. Requested by patient.
             </div>
-            <div className="appointment-time">Time: </div>
-            <div className="appointment-timeDetails">
+            <h4 className="appointment-subTitle">Time: </h4>
+            <div className="appointment-textContent">
               9:00AM Thursday 17 March, 2022
             </div>
-            <div className="appointment-location">Location:</div>
-            <div className="appointment-locationDetails">
-              St. Mary's Hospital
-            </div>
-            <div classname="appointment-note">Note:</div>
-            <div classname="appointment-noteDetails">
+            <h4 className="appointment-subTitle">Location:</h4>
+            <div className="appointment-textContent">St. Mary's Hospital</div>
+            <h4 className="appointment-subTitle">Note: </h4>
+            <div className="appointment-textContent">
               Please bring your health insurance card, personal ID, and related
               documents.
             </div>
 
             <div className="appointment-buttonContainer">
-              <Button type="submit" variant="contained">
+              <Button
+                type="submit"
+                variant="contained"
+                className="appointment-acceptButton"
+              >
                 ACCEPT
               </Button>
-              <Button type="submit" variant="contained">
+              <Button type="submit" variant="outlined">
                 DECLINE
               </Button>
             </div>
