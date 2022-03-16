d68470d5cd8ba340702d6f69713c3afbd3f75620
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2022-03-16 03:34:58 -0400

    Added comments to the QA test

--- Admin-app/cypress/integration/FinalQA.spec.js
+++ Admin-app/cypress/integration/FinalQA.spec.js
@@ -4,31 +4,76 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+
+
+/**
+ * @fileoverview This file contains the code for the QA test for the admin-app
+ * @Author: Mohammad Ali Zahir
+ */
+
 /* ==== Test Created with Cypress Studio ==== */
-/* ==== Test Created with Cypress Studio ==== */
+/**
+ * @param  {} 'QATest' name of the test 
+ * @param  {} function() name of the function
+ */
 it('QA Test', function() {
  /* ==== Generated with Cypress Studio ==== */
+
+ /**
+  * Accessing the webpage 
+  */
  cy.visit('http://localhost:3000/');
  cy.get('#email').clear();
+
+ /**
+  * Adding the email and password to the test
+  */
  cy.get('#email').type('QA-Admin@gmail.com');
  cy.get('#password').clear();
  cy.get('#password').type('admin12345{enter}');
  cy.get('.MuiButton-root').click();
+
+ /**
+  * Clicking the upcomingEvents button and the Covid News button, and the event button
+  */
  cy.get('.UPCOMING-EVENTS-1__background > :nth-child(4) > .MuiButton-root').click({force: true});
  cy.get(':nth-child(7) > .MuiBackdrop-root').click({force: true});
  cy.get('.UPCOMING-EVENTS-2__background > :nth-child(4) > .MuiButton-root').click({force: true});
  cy.get(':nth-child(7) > .MuiBackdrop-root').click({force: true});
  cy.get('.COVID-NEWS__text').click({force: true});
  cy.get('[href="/"] > .MuiButtonBase-root').click();
- cy.get('.EVENT-BTN__text').click();
+ cy.get('.EVENT-BTN__text').click(); 
+ 
+ /**
+  * Back to dashboard
+  */
+
  cy.get('[href="/"] > .MuiButtonBase-root').click();
+
+/**
+ * To the patients page
+ */
+
  cy.get('[href="/patients"] > .MuiButtonBase-root > .MuiListItemText-root > .MuiTypography-root').click();
  cy.get('[tabindex="0"] > [data-testid="KeyboardArrowRightIcon"]').click();
+
+ /**
+  * To the QR page
+  */
  cy.get('[href="/"] > .MuiButtonBase-root > .MuiListItemText-root > .MuiTypography-root').click();
  cy.get('[href="/QR"] > .MuiButtonBase-root').click();
+
+ /**
+  * To the inbox page
+  */
  cy.get('[href="/"] > .MuiButtonBase-root > .MuiListItemText-root > .MuiTypography-root').click();
  cy.get('[href="/testing"] > .MuiButtonBase-root > .MuiListItemText-root > .MuiTypography-root').click();
  cy.get('[href="/inbox"] > .MuiButtonBase-root > .MuiListItemText-root > .MuiTypography-root').click();
+
+ /**
+  * Back to the dashboard, and then finally log out
+  */
+
  cy.get('.MuiGrid-grid-xs-4 > .MuiList-root').click();
  cy.get('[href="/"] > .MuiButtonBase-root').click();
  cy.get('.MuiIconButton-edgeEnd > [data-testid="AccountCircleIcon"]').click();
--- Client-app/cypress/integration/FinalQA.spec.js
+++ Client-app/cypress/integration/FinalQA.spec.js
@@ -4,23 +4,49 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+
+/**
+ * @fileoverview This file contains the QA test for the client-app
+ * @Author: Mohammad Ali Zahir
+ */
+
 /* ==== Test Created with Cypress Studio ==== */
+/**
+ * @param  {} 'QATest' name of the test
+ * @param  {} function() name of the function
+ */
 it('QA Test', function() {
   /* ==== Generated with Cypress Studio ==== */
+  /**
+   * Accessing the localhost and logging in to the website
+   */
   cy.visit('localhost:3000');
   cy.get('#email').clear();
   cy.get('#email').type('qa-client@gmail.com');
   cy.get('#password').type('client12345{enter}');
   cy.get('.MuiButton-root').click();
+
+  /**
+   * Clicking to the appointment page
+   */
   cy.get(':nth-child(1) > a > .dashboard-card__container > .dashboard-card__img').click();
   cy.get('.appointment-buttonContainer > .MuiButton-contained').click();
   cy.get('.MuiButton-outlined').click();
   cy.get('.css-4p6bx2-MuiButtonBase-root-MuiIconButton-root > a').click();
   cy.get(':nth-child(2) > a > .dashboard-card__container').click();
+
+  /**
+   * Clicking around to go back to the dashboard , and seeing how the diary works
+   */
   cy.get('a > .BOTTOM-NAV__btn__title').click();
   cy.get(':nth-child(3) > a > .dashboard-card__container').click();
   cy.get(':nth-child(1) > :nth-child(1) > [sx="[object Object]"] > .MuiButtonBase-root > [data-testid="BookIcon"] > path').click();
   cy.get('.MuiDialogActions-root > .MuiButton-root').click();
+
+  /**
+   * Clicking around, and writing a message to the doctor
+   */
+
   cy.get('[data-testid="HomeRoundedIcon"] > path').click();
   cy.get(':nth-child(4) > a > .dashboard-card__container').click();
   cy.get('#mui-1').clear();
@@ -29,6 +55,11 @@ it('QA Test', function() {
   cy.get('.css-4p6bx2-MuiButtonBase-root-MuiIconButton-root > a').click();
   cy.get(':nth-child(5) > a > .dashboard-card__container > .dashboard-card__img').click();
   cy.get('a > .BOTTOM-NAV__btn__title').click();
+
+  /**
+   * Clicking around to find the button to update status
+   */
+
   cy.get(':nth-child(7) > a > .dashboard-card__container > .dashboard-card__title').click();
   cy.get('.css-4p6bx2-MuiButtonBase-root-MuiIconButton-root > a').click();
   cy.get(':nth-child(8) > a > .dashboard-card__container > .dashboard-card__img').click();
@@ -54,6 +85,11 @@ it('QA Test', function() {
   cy.get(':nth-child(10) > .MuiFormControlLabel-root > .MuiCheckbox-root > .PrivateSwitchBase-input').check();
   cy.get('.MuiBox-root > .MuiButton-root').click();
   cy.get('.MuiBackdrop-root').click({force:true});
+
+  /**
+   * Accessing the profile and then editing it
+   */
+
   cy.get('a > .BOTTOM-NAV__btn__title').click();
   cy.get('.client-profile > .MuiButtonBase-root').click();
   cy.get('[data-testid="EditIcon"]').click();
@@ -62,6 +98,11 @@ it('QA Test', function() {
   cy.get('#firstName').type('QA');
   cy.get('.MuiModal-root > .MuiBox-root').click();
   cy.get('.update-button').click();
+
+  /**
+   * Going to the notification screen and then finally logging out
+   */
+  
   cy.get('[data-testid="NotificationsIcon"]').click({force: true});
   cy.get('.MuiButton-root > .BOTTOM-NAV__btn__title').click();
   cy.get('[data-testid="signout"]').click();
