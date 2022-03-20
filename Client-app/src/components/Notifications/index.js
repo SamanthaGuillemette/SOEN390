aadd0a40abcb17f25b086b235b55c69f763f0734
Tushar Raval <tushar.raval102@gmail.com>
2022-03-20 13:32:16 -0400

    Added node-schedule to send notifcation at midnight

--- Client-app/src/components/Notifications/index.js
+++ Client-app/src/components/Notifications/index.js
@@ -10,9 +10,41 @@ import CoronavirusIcon from "@mui/icons-material/Coronavirus";
 import CloseIcon from "@mui/icons-material/Close";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { Divider } from "@mui/material";
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import { useEffect, useState, useRef } from "react";
+import { db, auth } from "../../backend/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import {
+  doc,
+  serverTimestamp,
+  addDoc,
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  updateDoc,
+  increment,
+} from "firebase/firestore";
 import "./Notifications.css";
 
 const Notifications = () => {
+
+  const [user] = useAuthState(auth);
+
+  const schedule = require('node-schedule');
+  const rule = new schedule.RecurrenceRule();
+
+  rule.dayOfWeek = [0, new schedule.Range(0, 7)];
+  rule.hour = 23;
+  rule.minute = 59;
+
+  const job = schedule.scheduleJob(rule, function () {
+    console.log('The time is 11:59');
+  });
+
+  // const clientRef = doc(db, `Client/${user?.email}`);
+  // const postalcodeRef = collection(clientRef, "postalCode");
+
   return (
     <>
       <div className="notifications-container ">
@@ -158,6 +190,45 @@ const Notifications = () => {
                   style={{
                     marginLeft: "50px",
                     marginBottom: "30px",
+                  }}
+                  color="#949be2"
+                >
+                  1 January, 2022. At 3:20 pm.
+                </Typography>
+                <Divider color="#949be2" />
+              </Box>
+              <Box>
+                <Box style={{
+                  marginTop: '20px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  flexWrap: 'wrap',
+                }}
+                >
+                  <ModeEditIcon color="primary" sx={{ fontSize: 40 }} />
+                  <Typography
+                    style={{
+                      marginLeft: '10px',
+                    }}
+                    color="var(--text-primary)"
+                  >
+                    <b>Status Update Reminder</b>
+                  </Typography>
+                  <CloseIcon className="notifications-closeIcon" />
+                </Box>
+                <Typography
+                  style={{
+                    marginLeft: '50px',
+                    marginBottom: '30px'
+                  }}
+                  color="var(--text-primary)"
+                >
+                  How are you feeling today? Please update your symptoms list for today.
+                </Typography>
+                <Typography
+                  style={{
+                    marginLeft: '50px',
+                    marginBottom: '30px'
                   }}
                   color="#949be2"
                 >
