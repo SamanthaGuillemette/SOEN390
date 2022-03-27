e1afc462afce97b5dae296c3989a989c16fc4023
Laila Alhalabi <laila.alhalabi99@gmail.com>
2022-03-27 14:11:24 -0400

    adjusted notifications box

--- Admin-app/src/components/Notifications/index.js
+++ Admin-app/src/components/Notifications/index.js
@@ -6,170 +6,186 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import CoronavirusIcon from '@mui/icons-material/Coronavirus';
-import CloseIcon from '@mui/icons-material/Close';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import ReportIcon from '@mui/icons-material/Report';
-import { Divider } from '@mui/material';
+import CoronavirusIcon from "@mui/icons-material/Coronavirus";
+import CloseIcon from "@mui/icons-material/Close";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import ReportIcon from "@mui/icons-material/Report";
+import { Divider } from "@mui/material";
 import "./Notifications.css";
 
 /**
  * This component is what allows the Notifications feature to work.
  */
 const Notifications = () => {
-    return (
-        <>
-            <div className="ADMIN-NOTIFICATIONS__container">
-                <Card className="ADMIN-NOTIFICATIONS__box">
-                    <CardContent
-                        sx={{
-                          display: "flex",
-                          flexDirection: "column",
-                          p: "30px",
-                          mb: "20px",
-                        }}
-                    >
-                        <Typography
-                            sx={{
-                                mx: "30px",
-                                pb: "30px",
-                                fontWeight: "800",
-                            }}
-                            color="var(--text-primary)"
-                            variant="h5"
-                        >
-                            Notifications
-                        </Typography>
-                        <Box>
-                            <Box>
-                                <Box style={{
-                                    marginTop: '20px',
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    flexWrap: 'wrap',
-                                }}
-                                >
-                                    <CheckCircleIcon color="success" sx={{ fontSize: 40 }} />
-                                    <Typography
-                                        style={{
-                                            marginLeft: '10px',
-                                        }}
-                                        color="var(--text-primary)"
-                                    >
-                                        <b>Status Update</b>
-                                    </Typography>
-                                    <CloseIcon className="NOTIFICATIONS__closingIcon"/>
-                                </Box>
-                                <Typography
-                                    style={{
-                                        marginLeft: '50px',
-                                        marginBottom: '20px'
-                                    }}
-                                    color="var(--text-primary)"
-                                >
-                                    Shakira updated her status. Please check the status update for more information.
-                                </Typography>
-                                <Typography
-                                    style={{
-                                        marginLeft: '50px',
-                                        marginBottom: '30px'
-                                    }}
-                                    color="#949be2"
-                                    data-testid="notification-statusUpdate"
-                                >
-                                    22 March, 2022. At 5:50 pm.
-                                </Typography>
-                                <Divider color="#949be2" />
-                            </Box>
-                        </Box>
-                        <Box>
-                            <Box>
-                                <Box style={{
-                                    marginTop: '20px',
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    flexWrap: 'wrap',
-                                }}
-                                >
-                                    <CoronavirusIcon color="error" sx={{ fontSize: 40 }} />
-                                    <Typography
-                                        style={{
-                                            marginLeft: '10px',
-                                        }}
-                                        color="var(--text-primary)"
-                                    >
-                                        <b>New Case Reported</b>
-                                    </Typography>
-                                    <CloseIcon className="NOTIFICATIONS__closingIcon " />
-                                </Box>
-                                <Typography
-                                    style={{
-                                        marginLeft: '50px',
-                                        marginBottom: '30px'
-                                    }}
-                                    color="var(--text-primary)"
-                                >
-                                    A new patient was tested positive, please check the patient's information and status for more information.
-                                </Typography>
-                                <Typography
-                                    style={{
-                                        marginLeft: '50px',
-                                        marginBottom: '30px'
-                                    }}
-                                    color="#949be2"
-                                >
-                                    22 January, 2022. At 4:20 pm.
-                                </Typography>
-                                <Divider color="#949be2" />
-                            </Box>
-                        </Box>
-                        <Box>
-                            <Box>
-                                <Box style={{
-                                    marginTop: '20px',
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    flexWrap: 'wrap',
-                                }}
-                                >
-                                    <ReportIcon color="error" sx={{ fontSize: 40 }} />
-                                    <Typography
-                                        style={{
-                                            marginLeft: '10px',
-                                        }}
-                                        color="var(--text-primary)"
-                                    >
-                                        <b>Urgent Case</b>
-                                    </Typography>
-                                    <CloseIcon className="NOTIFICATIONS__closingIcon" />
-                                </Box>
-                                <Typography
-                                    style={{
-                                        marginLeft: '50px',
-                                        marginBottom: '30px'
-                                    }}
-                                    color="var(--text-primary)"
-                                >
-                                    Your patient Micheal Kors is requesting an urgent status review. Please check the inbox messages for more information. 
-                                </Typography>
-                                <Typography
-                                    style={{
-                                        marginLeft: '50px',
-                                        marginBottom: '30px'
-                                    }}
-                                    color="#949be2"
-                                >
-                                    1 January, 2022. At 3:20 pm.
-                                </Typography>
-                                <Divider color="#949be2" />
-                            </Box>
-                        </Box>
-                    </CardContent>
-                </Card>
-            </div>
-        </>
-    );
+  const schedule = require("node-schedule");
+  const rule = new schedule.RecurrenceRule();
+
+  rule.dayOfWeek = [0, new schedule.Range(0, 7)];
+  rule.hour = 23;
+  rule.minute = 59;
+
+  const job = schedule.scheduleJob(rule, function () {
+    console.log("The time is 11:59");
+  });
+
+  return (
+    <>
+      <div className="ADMIN-NOTIFICATIONS__container">
+        <Card className="ADMIN-NOTIFICATIONS__box">
+          <CardContent
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              p: "30px",
+              mb: "20px",
+            }}
+          >
+            <Typography
+              sx={{
+                mx: "30px",
+                pb: "30px",
+                fontWeight: "800",
+              }}
+              color="var(--text-primary)"
+              variant="h5"
+            >
+              Notifications
+            </Typography>
+            <Box>
+              <Box>
+                <Box
+                  style={{
+                    marginTop: "20px",
+                    display: "flex",
+                    alignItems: "center",
+                    flexWrap: "wrap",
+                  }}
+                >
+                  <CheckCircleIcon color="success" sx={{ fontSize: 40 }} />
+                  <Typography
+                    style={{
+                      marginLeft: "10px",
+                    }}
+                    color="var(--text-primary)"
+                  >
+                    <b>Status Update</b>
+                  </Typography>
+                  <CloseIcon className="NOTIFICATIONS__closingIcon" />
+                </Box>
+                <Typography
+                  style={{
+                    marginLeft: "50px",
+                    marginBottom: "20px",
+                  }}
+                  color="var(--text-primary)"
+                >
+                  Shakira updated her status. Please check the status update for
+                  more information.
+                </Typography>
+                <Typography
+                  style={{
+                    marginLeft: "50px",
+                    marginBottom: "30px",
+                  }}
+                  color="#949be2"
+                  data-testid="notification-statusUpdate"
+                >
+                  22 March, 2022. At 5:50 pm.
+                </Typography>
+                <Divider color="#949be2" />
+              </Box>
+            </Box>
+            <Box>
+              <Box>
+                <Box
+                  style={{
+                    marginTop: "20px",
+                    display: "flex",
+                    alignItems: "center",
+                    flexWrap: "wrap",
+                  }}
+                >
+                  <CoronavirusIcon color="error" sx={{ fontSize: 40 }} />
+                  <Typography
+                    style={{
+                      marginLeft: "10px",
+                    }}
+                    color="var(--text-primary)"
+                  >
+                    <b>New Case Reported</b>
+                  </Typography>
+                  <CloseIcon className="NOTIFICATIONS__closingIcon " />
+                </Box>
+                <Typography
+                  style={{
+                    marginLeft: "50px",
+                    marginBottom: "30px",
+                  }}
+                  color="var(--text-primary)"
+                >
+                  A new patient was tested positive, please check the patient's
+                  information and status for more information.
+                </Typography>
+                <Typography
+                  style={{
+                    marginLeft: "50px",
+                    marginBottom: "30px",
+                  }}
+                  color="#949be2"
+                >
+                  22 January, 2022. At 4:20 pm.
+                </Typography>
+                <Divider color="#949be2" />
+              </Box>
+            </Box>
+            <Box>
+              <Box>
+                <Box
+                  style={{
+                    marginTop: "20px",
+                    display: "flex",
+                    alignItems: "center",
+                    flexWrap: "wrap",
+                  }}
+                >
+                  <ReportIcon color="error" sx={{ fontSize: 40 }} />
+                  <Typography
+                    style={{
+                      marginLeft: "10px",
+                    }}
+                    color="var(--text-primary)"
+                  >
+                    <b>Urgent Case</b>
+                  </Typography>
+                  <CloseIcon className="NOTIFICATIONS__closingIcon" />
+                </Box>
+                <Typography
+                  style={{
+                    marginLeft: "50px",
+                    marginBottom: "30px",
+                  }}
+                  color="var(--text-primary)"
+                >
+                  Your patient Micheal Kors is requesting an urgent status
+                  review. Please check the inbox messages for more information.
+                </Typography>
+                <Typography
+                  style={{
+                    marginLeft: "50px",
+                    marginBottom: "30px",
+                  }}
+                  color="#949be2"
+                >
+                  1 January, 2022. At 3:20 pm.
+                </Typography>
+                <Divider color="#949be2" />
+              </Box>
+            </Box>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
 };
 
 export default Notifications;
-
