48cfa10529132d3be8d7390618365a4c64b56ea9
Marwa Khalid <khalidmarwa786@gmail.com>
2022-03-28 16:56:16 -0400

    Fixed doughnut chart and changed id to email

--- Admin-app/src/components/Charts/DoughnutChart.js
+++ Admin-app/src/components/Charts/DoughnutChart.js
@@ -4,25 +4,27 @@
  */
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
-import { useEffect, useState} from "react";
-import { getDoctors, patientLimit } from "../../backend/firebaseDoctorUtilities";
+import { useEffect, useState } from "react";
+import {
+  getDoctors,
+  patientLimit,
+} from "../../backend/firebaseDoctorUtilities";
 import Typography from "@mui/material/Typography";
 import "./DoughtnutChart.css";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
-
 
 /**
  * This function will check the capacity of the doctor.
  *
  * @param  {} doctor
  */
- function isDoctorAtFullCapacity(doctor) {
+function isDoctorAtFullCapacity(doctor) {
   return doctor && doctor.treats && doctor.treats.length >= patientLimit;
 }
 
 /**
- * This component allows displaying the Doughnut. 
+ * This component allows displaying the Doughnut.
  *
  * @returns {JSX.Element}
  */
@@ -30,17 +32,18 @@ function DoughnutChart() {
   const [doctorsAtCapacity, setDoctorsAtCapacity] = useState(0);
   const [doctorsAvailable, setDoctorsAvailable] = useState(0);
 
-    /* Counting how many doctors are at full capacity and how many arent  */
-    useEffect(() => {
-      getDoctors().then((data) => {
-        data.forEach((doc) => {
-          if (!isDoctorAtFullCapacity(doc)) { /* if NOT at full capacity */
-            setDoctorsAvailable(doctorsAvailable => doctorsAvailable + 1)
-          } else {
-            setDoctorsAtCapacity(doctorsAtCapacity => doctorsAtCapacity + 1)
-          }
-        });
+  /* Counting how many doctors are at full capacity and how many arent  */
+  useEffect(() => {
+    getDoctors().then((data) => {
+      data.forEach((doc) => {
+        if (!isDoctorAtFullCapacity(doc)) {
+          /* if NOT at full capacity */
+          setDoctorsAvailable((doctorsAvailable) => doctorsAvailable + 1);
+        } else {
+          setDoctorsAtCapacity((doctorsAtCapacity) => doctorsAtCapacity + 1);
+        }
       });
+    });
   }, []);
 
   const data = {
@@ -51,20 +54,32 @@ function DoughnutChart() {
         backgroundColor: ["#8bc3eb", "rgb(5, 132, 190)"],
         borderColor: ["#1e1e1e"],
         borderWidth: 3,
-        cutout: '80%',
-        borderRadius: 15,
+        cutout: "80%",
         hoverBackgroundColor: ["#8bc3eb", "rgb(5, 132, 190)"],
       },
     ],
   };
 
   return (
-    <div style={{ maxWidth: "400px"}}>
-      <Typography sx={{fontSize: "18px", color: "var(--text-primary)", fontWeight: "bold", textAlign: "center"}}>Doctors Available vs Unavailable</Typography>
-      <Doughnut data={data}/>
-      <Typography sx={{textAlign: "center", mt: 1}}>
-        <Typography className="DASHBOARD__chart__label-unavail">Unavailable</Typography>
-        <Typography className="DASHBOARD__chart__label-avail">Available</Typography>
+    <div style={{ maxWidth: "400px" }}>
+      <Typography
+        sx={{
+          fontSize: "18px",
+          color: "var(--text-primary)",
+          fontWeight: "bold",
+          textAlign: "center",
+        }}
+      >
+        Doctors Available vs Unavailable
+      </Typography>
+      <Doughnut data={data} />
+      <Typography sx={{ textAlign: "center", mt: 1 }}>
+        <Typography className="DASHBOARD__chart__label-unavail">
+          Unavailable
+        </Typography>
+        <Typography className="DASHBOARD__chart__label-avail">
+          Available
+        </Typography>
       </Typography>
     </div>
   );
