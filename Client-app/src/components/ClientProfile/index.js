95e8314fa107d9a51a3fe53bb64f0c416db0d9d5
Marwa Khalid <khalidmarwa786@gmail.com>
2022-03-27 20:07:41 -0400

    Removed Status box & added sign out

--- Client-app/src/components/ClientProfile/index.js
+++ Client-app/src/components/ClientProfile/index.js
@@ -9,25 +9,10 @@ import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
-import Paper from "@mui/material/Paper";
-import Divider from "@mui/material/Divider";
-import Stack from "@mui/material/Stack";
-import { styled } from "@mui/material/styles";
-import FlagIcon from "@mui/icons-material/Flag";
-import { useState } from "react";
 import EditModal from "./ProfileEditModal";
 import { useSelector } from "react-redux";
 
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(0.5),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
-
 function ClientProfile() {
-  const [priorityFlag, setPriorityFlag] = useState(false);
 
   /**
    * Pull 'userInfoDetails' from the store (Redux centralized store)
@@ -38,7 +23,7 @@ function ClientProfile() {
 
   return (
     <Box className="clientProfile-container">
-      <Grid container spacing={3}>
+      <Grid container spacing={5}>
         <Grid item xs={12}>
           <Card>
             <Box className="clientProfile-profileCard">
@@ -85,65 +70,6 @@ function ClientProfile() {
               </CardContent>
             </Box>
           </Card>
-        </Grid>
-        <Grid item xs={12}>
-          <Grid item>
-            <Card
-              className={priorityFlag ? "status-card clicked" : "status-card"}
-            >
-              <CardActionArea>
-                <CardContent>
-                  <div className="clientProfile-statusBox">
-                    <Typography
-                      className="profile__header"
-                      gutterBottom
-                      variant="button"
-                      component="div"
-                    >
-                      Status
-                      <FlagIcon
-                        onClick={() => {
-                          priorityFlag
-                            ? setPriorityFlag(false)
-                            : setPriorityFlag(true);
-                        }}
-                        className={
-                          priorityFlag
-                            ? "clientProfile-priority-flag clicked"
-                            : "clientProfile-priority-flag"
-                        }
-                      ></FlagIcon>
-                    </Typography>
-                  </div>
-                  <Stack
-                    direction="row"
-                    divider={<Divider orientation="vertical" />}
-                    spacing={1}
-                    marginBottom={2}
-                    alignItems="baseline"
-                  >
-                    <span className={userInfoDetails?.status === "POSITIVE" ? "PATIENT__label-positive" : userInfoDetails?.status === "NEGATIVE" ? "PATIENT__label-negative" : "PATIENT__label-unconfirmed"}>
-                      {userInfoDetails?.status}</span>
-                  </Stack>
-                  <Box />
-                  <Stack spacing={2}>
-                    <Item
-                      className="profile-data"
-                      sx={{ bgcolor: "inherit", boxShadow: "none" }}
-                    >
-                      Temperature: 39 °C
-                    </Item>
-                    <Item
-                      className="profile-data"
-                      sx={{ bgcolor: "inherit", boxShadow: "none" }}
-                    >
-                      Weight: 150 lbs
-                    </Item>
-                  </Stack>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
         </Grid>
       </Grid>
     </Box>
