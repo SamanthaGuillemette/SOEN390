e6d85533efc0376bcde807c970f4d71e7598eb1b
Marwa Khalid <khalidmarwa786@gmail.com>
2022-03-28 02:44:44 -0400

    Removed unused import

--- Client-app/src/components/AppBody/index.js
+++ Client-app/src/components/AppBody/index.js
@@ -6,31 +6,30 @@ import Box from "@mui/material/Box";
 import BottomNav from "../BottomNav";
 import MenuAppBar from "../Navbar";
 import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
 
 /**
  * This component returns the app body containing the Sidebar, Navbar and Bottom Navbar components.
  * @returns {JSX.Element}
  */
 
- function AppBody(props) {
-     return (
-        <Box sx={{ display: "flex" }}>
-            <Box
-            component="main"
-            sx={{
-                flexGrow: 1,
-                height: "100vh",
-                overflow: "auto",
-            }}
-            >
-            <MenuAppBar/>
-            <BottomNav/>
-            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-                {props.children}
-            </Container>
-            </Box>
-        </Box>
-     );
- }
- export default AppBody;
+function AppBody(props) {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          height: "100vh",
+          overflow: "auto",
+        }}
+      >
+        <MenuAppBar />
+        <BottomNav />
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          {props.children}
+        </Container>
+      </Box>
+    </Box>
+  );
+}
+export default AppBody;
