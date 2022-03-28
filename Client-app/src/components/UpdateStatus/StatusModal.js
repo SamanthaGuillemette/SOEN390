0a5a65613cb7be63ba69113adcfeda3518563361
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2022-03-28 04:37:58 -0400

    Fixed small mistakes

--- Client-app/src/components/UpdateStatus/StatusModal.js
+++ Client-app/src/components/UpdateStatus/StatusModal.js
@@ -8,9 +8,9 @@ import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import { useState } from "react";
 import { Grid, TextField } from "@mui/material";
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 import { useSelector } from "react-redux";
 import { db } from "../../backend/firebase";
 import { doc, collection, addDoc, serverTimestamp } from "firebase/firestore";
@@ -39,18 +39,19 @@ const theme = createTheme({
 });
 
 // This const does styling of the empty input field helper text
-const helperTextStyles = makeStyles(theme => ({
+const helperTextStyles = makeStyles((theme) => ({
   root: {
     "&.MuiFormHelperText-root.Mui-error": {
       color: "#d93025",
       fontSize: "12px",
-    }
-  }
+    },
+  },
 }));
 
 export default function SimpleModal() {
-  
-  const userInfoDetails = useSelector((state) => state.userInfo.userInfoDetails);
+  const userInfoDetails = useSelector(
+    (state) => state.userInfo.userInfoDetails
+  );
 
   // Pull 'userEmail' out from the centralized store
   const userEmail = useSelector((state) => state.auth.userEmail);
@@ -60,7 +61,6 @@ export default function SimpleModal() {
 
   // Get the doctor's reference via the assigned doctor (query the database)
   const adminDoc = doc(db, `Admin/${userInfoDetails?.assignedDoctor}`);
-
 
   const [openModal, setOpenModal] = useState(false);
   const [emptyFields, setEmptyFields] = useState(false);
@@ -78,14 +78,22 @@ export default function SimpleModal() {
   // Handle the popup open/close state
   const handleOpen = () => setOpenModal(true);
   const currentDate = new Date();
-  const formattedMonth = currentDate.getMonth() + 1 < 10 ? `0${currentDate.getMonth()}` : currentDate.getMonth();
-  const formattedDay = currentDate.getDate() + 1 < 10 ? `0${currentDate.getDate()}` : currentDate.getDate();
-  const formattedDate = formattedMonth + "/" + formattedDay + "/" + currentDate.getFullYear();
+  const formattedMonth =
+    currentDate.getMonth() + 1 < 10
+      ? `0${currentDate.getMonth()}`
+      : currentDate.getMonth();
+  const formattedDay =
+    currentDate.getDate() + 1 < 10
+      ? `0${currentDate.getDate()}`
+      : currentDate.getDate();
+  const formattedDate =
+    formattedMonth + "/" + formattedDay + "/" + currentDate.getFullYear();
 
   const handleSymptomsSubmit = async (event) => {
     event.preventDefault();
 
-    if (temperature !== "" && weight !== "") { // if they are filled
+    if (temperature !== "" && weight !== "") {
+      // if they are filled
       // then only adding new doc
       await addDoc(collection(clientDoc, "Status"), {
         temperature: temperature,
@@ -101,9 +109,10 @@ export default function SimpleModal() {
       });
 
       await addDoc(collection(adminDoc, "StatusNotifications"), {
-        patientName: userInfoDetails?.firstName + " " + userInfoDetails?.lastName,
-        patientID: userInfoDetails?.userEmail,
-        viewed: false,
+        patientName:
+          userInfoDetails?.firstName + " " + userInfoDetails?.lastName,
+        patientID: userInfoDetails?.email,
+        viewed: "false",
         timestamp: serverTimestamp(),
       });
 
@@ -127,88 +136,189 @@ export default function SimpleModal() {
     setSmellLoss(false);
     setMuscleAche(false);
     setTasteLoss(false);
-  }
+  };
 
   return (
-    <div> 
-      <Button onClick={handleOpen} className='updateStatus-button'><AddCircleIcon></AddCircleIcon></Button>
+    <div>
+      <Button onClick={handleOpen} className="updateStatus-button">
+        <AddCircleIcon></AddCircleIcon>
+      </Button>
       <ThemeProvider theme={theme}>
-      <Modal
-        open={openModal}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box
-          sx={style}
-          component="form"
-          noValidate
-          onSubmit={handleSymptomsSubmit}
+        <Modal
+          open={openModal}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
         >
-          <Typography className="header-statusModal" variant="h6" component="h2" sx={{ mb: 2.5 }}>
-            ADD STATUS
-          </Typography>
-          <Grid container minWidth={285} spacing={1}>
-             {/* Temperature TextField */}
-            <Grid item xs={12}>
-              <TextField
-                id="statusModal-standardBasic"
-                placeholder="Temperature (°C)"
-                variant="standard"
-                color="grey"
-                onChange={(e) => setTemperature(e.target.value)}
-                helperText={temperature === "" && emptyFields ? "This field is required." : ""}
-                error={temperature === "" && emptyFields}
-                FormHelperTextProps={{ classes: helperTestClasses }}
-              />
-            </Grid>
-             {/* Weight TextField */}
-            <Grid item xs={12}>
-              <TextField
-                id="statusModal-standardBasic"
-                placeholder="Weight (lbs)"
-                variant="standard"
-                color="grey"
-                onChange={(e) => setWeight(e.target.value)}
-                helperText={weight === "" && emptyFields ? "This field is required." : ""}
-                error={weight === "" && emptyFields}
-                FormHelperTextProps={{ classes: helperTestClasses }}
-              />
-            </Grid>
-            
-            {/* Checkboxes for symptoms */}
-            <Grid item style = {{maxWidth: "160px"}} xs={6} sm={6}><br/>
-              <FormControlLabel control={<Checkbox style ={{color: "lightskyblue"}} onChange={(e) => setFever(e.target.checked)} /> } label={<Typography variant="subtitle2" style={{ color: "white" }}>Fever</Typography>}/>
-            </Grid>
-            <Grid item style = {{maxWidth: "160px"}} xs={6} sm={6}><br/>
-              <FormControlLabel control={<Checkbox style ={{color: "lightskyblue"}} onChange={(e) => setSoreThroat(e.target.checked)} /> } label={<Typography variant="subtitle2" style={{ color: "white" }}>Sore Throat</Typography>}/>
-            </Grid>
-            <Grid item style = {{maxWidth: "160px"}} xs={6}  sm={6}>
-              <FormControlLabel control={<Checkbox style ={{color: "lightskyblue"}} onChange={(e) => setCough(e.target.checked)} /> } label={<Typography variant="subtitle2" style={{ color: "white" }}>Cough</Typography>}/>
-            </Grid>
-            <Grid item style = {{maxWidth: "160px"}} xs={6}  sm={6}>
-              <FormControlLabel control={<Checkbox style ={{color: "lightskyblue"}} onChange={(e) => setRunnyNose(e.target.checked)} /> } label={<Typography variant="subtitle2" style={{ color: "white" }}>Runny Nose</Typography>}/>
-            </Grid>
-            <Grid item style = {{maxWidth: "160px"}} xs={6}  sm={6}>
-              <FormControlLabel control={<Checkbox style ={{color: "lightskyblue"}} onChange={(e) => setSmellLoss(e.target.checked)} /> } label={<Typography variant="subtitle2" style={{ color: "white" }}>Smell Loss</Typography>}/>
-            </Grid>
-            <Grid item style = {{maxWidth: "160px"}} xs={6}  sm={6}>
-              <FormControlLabel control={<Checkbox style ={{color: "lightskyblue"}} onChange={(e) => setMuscleAche(e.target.checked)} /> } label={<Typography variant="subtitle2" style={{ color: "white" }}>Muscle Ache</Typography>}/>
-            </Grid>
-            <Grid item style = {{maxWidth: "160px"}} xs={6}  sm={6}>
-              <FormControlLabel control={<Checkbox style ={{color: "lightskyblue"}} onChange={(e) => setTasteLoss(e.target.checked)} /> } label={<Typography variant="subtitle2" style={{ color: "white" }}>Taste Loss</Typography>}/>
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            variant="contained"
-            className="updateStatus__save-btn"
-            sx={{ mt: 4, mb: 2}}
+          <Box
+            sx={style}
+            component="form"
+            noValidate
+            onSubmit={handleSymptomsSubmit}
           >
-            SAVE
-          </Button>
-        </Box>
-      </Modal>
+            <Typography
+              className="header-statusModal"
+              variant="h6"
+              component="h2"
+              sx={{ mb: 2.5 }}
+            >
+              ADD STATUS
+            </Typography>
+            <Grid container minWidth={285} spacing={1}>
+              {/* Temperature TextField */}
+              <Grid item xs={12}>
+                <TextField
+                  id="statusModal-standardBasic"
+                  placeholder="Temperature (°C)"
+                  variant="standard"
+                  color="grey"
+                  onChange={(e) => setTemperature(e.target.value)}
+                  helperText={
+                    temperature === "" && emptyFields
+                      ? "This field is required."
+                      : ""
+                  }
+                  error={temperature === "" && emptyFields}
+                  FormHelperTextProps={{ classes: helperTestClasses }}
+                />
+              </Grid>
+              {/* Weight TextField */}
+              <Grid item xs={12}>
+                <TextField
+                  id="statusModal-standardBasic"
+                  placeholder="Weight (lbs)"
+                  variant="standard"
+                  color="grey"
+                  onChange={(e) => setWeight(e.target.value)}
+                  helperText={
+                    weight === "" && emptyFields
+                      ? "This field is required."
+                      : ""
+                  }
+                  error={weight === "" && emptyFields}
+                  FormHelperTextProps={{ classes: helperTestClasses }}
+                />
+              </Grid>
+
+              {/* Checkboxes for symptoms */}
+              <Grid item style={{ maxWidth: "160px" }} xs={6} sm={6}>
+                <br />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      style={{ color: "lightskyblue" }}
+                      onChange={(e) => setFever(e.target.checked)}
+                    />
+                  }
+                  label={
+                    <Typography variant="subtitle2" style={{ color: "white" }}>
+                      Fever
+                    </Typography>
+                  }
+                />
+              </Grid>
+              <Grid item style={{ maxWidth: "160px" }} xs={6} sm={6}>
+                <br />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      style={{ color: "lightskyblue" }}
+                      onChange={(e) => setSoreThroat(e.target.checked)}
+                    />
+                  }
+                  label={
+                    <Typography variant="subtitle2" style={{ color: "white" }}>
+                      Sore Throat
+                    </Typography>
+                  }
+                />
+              </Grid>
+              <Grid item style={{ maxWidth: "160px" }} xs={6} sm={6}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      style={{ color: "lightskyblue" }}
+                      onChange={(e) => setCough(e.target.checked)}
+                    />
+                  }
+                  label={
+                    <Typography variant="subtitle2" style={{ color: "white" }}>
+                      Cough
+                    </Typography>
+                  }
+                />
+              </Grid>
+              <Grid item style={{ maxWidth: "160px" }} xs={6} sm={6}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      style={{ color: "lightskyblue" }}
+                      onChange={(e) => setRunnyNose(e.target.checked)}
+                    />
+                  }
+                  label={
+                    <Typography variant="subtitle2" style={{ color: "white" }}>
+                      Runny Nose
+                    </Typography>
+                  }
+                />
+              </Grid>
+              <Grid item style={{ maxWidth: "160px" }} xs={6} sm={6}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      style={{ color: "lightskyblue" }}
+                      onChange={(e) => setSmellLoss(e.target.checked)}
+                    />
+                  }
+                  label={
+                    <Typography variant="subtitle2" style={{ color: "white" }}>
+                      Smell Loss
+                    </Typography>
+                  }
+                />
+              </Grid>
+              <Grid item style={{ maxWidth: "160px" }} xs={6} sm={6}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      style={{ color: "lightskyblue" }}
+                      onChange={(e) => setMuscleAche(e.target.checked)}
+                    />
+                  }
+                  label={
+                    <Typography variant="subtitle2" style={{ color: "white" }}>
+                      Muscle Ache
+                    </Typography>
+                  }
+                />
+              </Grid>
+              <Grid item style={{ maxWidth: "160px" }} xs={6} sm={6}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      style={{ color: "lightskyblue" }}
+                      onChange={(e) => setTasteLoss(e.target.checked)}
+                    />
+                  }
+                  label={
+                    <Typography variant="subtitle2" style={{ color: "white" }}>
+                      Taste Loss
+                    </Typography>
+                  }
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              variant="contained"
+              className="updateStatus__save-btn"
+              sx={{ mt: 4, mb: 2 }}
+            >
+              SAVE
+            </Button>
+          </Box>
+        </Modal>
       </ThemeProvider>
     </div>
   );
