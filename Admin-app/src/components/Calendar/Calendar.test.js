a9c0f39195b711f4119224124361855d9b576dc3
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2022-04-03 00:29:15 -0400

    Fixed calendar test

--- Admin-app/src/components/Calendar/Calendar.test.js
+++ Admin-app/src/components/Calendar/Calendar.test.js
@@ -1,19 +1,19 @@
 import { render, screen, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Calendar from ".";
+import { Provider } from "react-redux";
 
 test("Calendar component has month-week-day buttons", () => {
-  render(<Calendar />);
-
-  // Check that the text content is there and if a we get a value by role if its there or not as well.
-  const monthButton = screen.getByRole("button", { name: /month view/i });
-  expect(monthButton.textContent).toContain("month");
-
-  const weekButton = screen.getByRole("button", { name: /week view/i });
-  expect(weekButton.textContent).toContain("week");
-
-  const dayButton = screen.getByRole("button", { name: /day view/i });
-  expect(dayButton.textContent).toContain("day");
+  <Provider>
+    render(
+    <Calendar />
+    ); const monthButton = screen.getByRole("button", {/month view/i});
+    expect(monthButton.textContent).toContain("month"); const weekButton =
+    screen.getByRole("button", {/week view/i});
+    expect(weekButton.textContent).toContain("week"); const dayButton =
+    screen.getByRole("button", {/day view/i});
+    expect(dayButton.textContent).toContain("day");
+  </Provider>;
 });
 
 afterEach(() => {
