c3452b2f12a642639afaf5ccbf4eaccd2ca8c5aa
salehatrq <68137308+salehatrq@users.noreply.github.com>
2022-04-11 17:19:48 -0400

    Added completed test for SymptomsTable

--- Client-app/src/components/SymptomsTable/SymptomsTable.test.js
+++ Client-app/src/components/SymptomsTable/SymptomsTable.test.js
@@ -1,8 +1,14 @@
-import { screen, render, cleanup } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
+import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import * as redux from 'react-redux';
 import SymptomsTable from "./index";
 
-test("should render the bottom navbar", () => {
-  //render(<SymptomsTable />);
+test("Symptoms Table should render without errors", () => {
+  const spy = jest.spyOn(redux, 'useSelector')
+  spy.mockReturnValue({ test:'test' })
+  render(<SymptomsTable />);
+});
+
+afterEach(() => {
+  cleanup();
 });
