5a5296697a6ba2bf0e0b3dc5f4dddba18de98f9c
salehatrq <68137308+salehatrq@users.noreply.github.com>
2022-04-11 19:23:12 -0400

    Fixed NavBar test

--- Client-app/src/components/Navbar/NavBar.test.js
+++ Client-app/src/components/Navbar/NavBar.test.js
@@ -1,13 +1,20 @@
 import { screen, render, cleanup } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
+import * as redux from "react-redux";
 import Navbar from "./index";
 
+const spy = jest.spyOn(redux, "useSelector");
+spy.mockReturnValue({ test: "test" });
+
+const spy2 = jest.spyOn(redux, "useDispatch");
+spy2.mockReturnValue({ test: "test" });
+
 test("should render the top navbar", () => {
-  // render(<MemoryRouter><Navbar /></MemoryRouter>);
-  // // Checks if navbar has been rendered
-  // const title = screen.getByTestId("title");
-  // expect(title).toBeInTheDocument();
-  // expect(title).toHaveTextContent("Covid-19 App");
-  // expect(title).toBeVisible();
+  render(<MemoryRouter><Navbar /></MemoryRouter>);
+  // Checks if navbar has been rendered
+  const title = screen.getByTestId("title");
+  expect(title).toBeInTheDocument();
+  expect(title).toHaveTextContent("Covid-19 App");
+  expect(title).toBeVisible();
 });
