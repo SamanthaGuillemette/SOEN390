74909835daf5fd7944ad7a4eef292568df09d1f0
AliZ786 <59709752+AliZ786@users.noreply.github.com>
2022-04-11 19:12:37 -0400

    Added AppBody test

---
+++ Client-app/src/components/AppBody/AppBody.test.js
@@ -0,0 +1,19 @@
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import * as redux from "react-redux";
+import { MemoryRouter } from "react-router-dom";
+import AppBody from "./index";
+
+const spy = jest.spyOn(redux, "useSelector");
+spy.mockReturnValue({ test: "test" });
+
+const spy2 = jest.spyOn(redux, "useDispatch");
+spy2.mockReturnValue({ test: "test" });
+
+test("should render app body", () => {
+  render(
+    <MemoryRouter>
+      <AppBody />
+    </MemoryRouter>
+  );
+});
