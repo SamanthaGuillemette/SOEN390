dbbd86f05567902d692137a3474e4c2089b47a50
salehatrq <68137308+salehatrq@users.noreply.github.com>
2022-04-11 18:55:27 -0400

    Completed News and DiaryTable tests

--- Client-app/src/components/Diary/DiaryTable.test.js
+++ Client-app/src/components/Diary/DiaryTable.test.js
@@ -2,16 +2,24 @@ import { screen, render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import * as redux from 'react-redux';
 import DiaryTable from "./index";
+import DiaryAddModal from "./DiaryAddModal";
+
+const spy = jest.spyOn(redux, 'useSelector')
+spy.mockReturnValue({ test:'test' })
 
 test("should render the diary table", () => {
-  const spy = jest.spyOn(redux, 'useSelector')
-  spy.mockReturnValue({ test:'test' })
   render(
   <DiaryTable />);
   const diaryList = screen.getByTestId("diary-list");
   expect(diaryList).toBeInTheDocument();
   expect(diaryList).toHaveClass("diary__list");
 });
+
+test("should render the diary add modal", () => {
+  render(
+  <DiaryAddModal />);
+});
+
 
 afterEach(() => {
   cleanup();
